3d88c9db020bd89516ab45437f3e8cbd34d4c309
Paolo Galli <49845775+Paolo97Gll@users.noreply.github.com>
2021-03-10 05:17:29 +0100

    Add tests for + and - operators

--- Raytracer/test/runtests.jl
+++ Raytracer/test/runtests.jl
@@ -1,7 +1,13 @@
 using Raytracer   # Mettete il nome che avete scelto
 using Test
+using ColorTypes.RGB
+
+c1 = RGB(.3, .6, .7)
+c2 = RGB(.4, .5, .8)
 
 @testset "Colors" begin
     # Put here the tests required for color sum and product
     @test 1 + 1 == 2
+    @test c1 + c2 ≈ RGB(.7, 1.1, 1.5)
+    @test c1 - c2 ≈ RGB(-.1, .1, -.1)
 end
