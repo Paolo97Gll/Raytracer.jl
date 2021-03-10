bf2bb96cbd1c5b403629e63090dba94daebb0be1
Samuele-Colombo <samuele.colombo1@studenti.unimi.it>
2021-03-10 12:02:44 +0100

    subtituted using with import

--- Raytracer/test/runtests.jl
+++ Raytracer/test/runtests.jl
@@ -1,14 +1,14 @@
 using Raytracer   # Mettete il nome che avete scelto
 using Test
-using ColorTypes.RGB
+import ColorTypes.RGB
 
 @testset "Colors" begin
     @test 1 + 1 == 2
-    let c1 = ColorTypes.RGB(1., 2., 3.), c2 = ColorTypes.RGB(.4, .5, .6)
+    let c1 = RGB(1., 2., 3.), c2 = RGB(.4, .5, .6)
         a = 2
         #test scalar multiplication
-        @test a * c1 == ColorTypes.RGB(2., 4., 6.) 
-        @test c1 * a == ColorTypes.RGB(2., 4., 6.) 
+        @test a * c1 == RGB(2., 4., 6.) 
+        @test c1 * a == RGB(2., 4., 6.) 
         @test a * c1 == c1 * a
 
         #test elementwise ≈
@@ -16,10 +16,10 @@ using ColorTypes.RGB
         for i in 1:10
             a += .1
         end
-        @test ColorTypes.RGB(a, 2. * a, 3. * a) ≈ c1
+        @test RGB(a, 2. * a, 3. * a) ≈ c1
 
         #test elementwise multiplication
-        @test c1 * c2 ≈ ColorTypes.RGB(1. * .4, 2. * .5, 3. * .6)
+        @test c1 * c2 ≈ RGB(1. * .4, 2. * .5, 3. * .6)
     end
 
     let c1 = RGB(.3, .6, .7), c2 = RGB(.4, .5, .8)
