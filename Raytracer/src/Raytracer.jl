12eb7c48dea0f2f5e96848aa3349190838a1963b
Samuele-Colombo <samuele.colombo1@studenti.unimi.it>
2021-03-10 11:53:42 +0100

    Add scalar multiplication, elementwise multiplication and elementwise \approx dispatches

--- Raytracer/src/Raytracer.jl
+++ Raytracer/src/Raytracer.jl
@@ -1,9 +1,20 @@
 module Raytracer
 
-import ColorTypes
+import ColorTypes.RGB
 import Base.:+, Base.:*, Base.:≈
 
-# To make this work, first define the product "scalar * color"
-Base.:*(c::ColorTypes.RGB{T}, scalar) where {T} = scalar * c
+"""Dispatch of scalar multiplication for a RGB type instance"""
+Base.:*(scalar::Number, c::RGB{T}) where {T} = RGB{T}((scalar .* (c.r, c.g, c.b))...)
+
+"""Mirrored version of scalar multiplication for a RGB type instance"""
+Base.:*(c::RGB{T}, scalar::Number) where {T} = scalar * c
+
+"""Dispatch of elementwise multiplication between two RGB type instances"""
+Base.:*(c1::RGB{T}, c2::RGB{T}) where {T} = RGB{T}(((c1.r, c1.g, c1.b) .* (c2.r, c2.g, c2.b))...)
+
+"""Dispatch of elementwise ≈ operator between two RGB type instances"""
+Base.:≈(c1::RGB{T}, c2::RGB{T}) where {T} = c1.r ≈ c2.r &&
+                                            c1.g ≈ c2.g &&
+                                            c1.b ≈ c2.b
 
 end # module
--- Raytracer/test/runtests.jl
+++ Raytracer/test/runtests.jl
@@ -1,7 +1,24 @@
 using Raytracer   # Mettete il nome che avete scelto
 using Test
+import ColorTypes
 
 @testset "Colors" begin
-    # Put here the tests required for color sum and product
     @test 1 + 1 == 2
+    c1 = ColorTypes.RGB(1., 2., 3.)
+    c2 = ColorTypes.RGB(.4, .5, .6)
+    a = 2
+    #test scalar multiplication
+    @test a * c1 == ColorTypes.RGB(2., 4., 6.) 
+    @test c1 * a == ColorTypes.RGB(2., 4., 6.) 
+    @test a * c1 == c1 * a
+
+    #test elementwise ≈
+    a = 0
+    for i in 1:10
+        a += .1
+    end
+    @test ColorTypes.RGB(a, 2. * a, 3. * a) ≈ c1
+
+    #test elementwise multiplication
+    @test c1 * c2 ≈ ColorTypes.RGB(1. * .4, 2. * .5, 3. * .6)
 end
