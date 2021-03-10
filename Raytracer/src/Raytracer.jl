31e8960b474c5e2dbc7413495452c5f1bc28280b
Paolo Galli <49845775+Paolo97Gll@users.noreply.github.com>
2021-03-10 04:18:37 +0100

    Add overloading template

--- Raytracer/src/Raytracer.jl
+++ Raytracer/src/Raytracer.jl
@@ -1,5 +1,9 @@
 module Raytracer
 
-greet() = print("Hello World!")
+import ColorTypes
+import Base.:+, Base.:*, Base.:≈
+
+# To make this work, first define the product "scalar * color"
+Base.:*(c::ColorTypes.RGB{T}, scalar) where {T} = scalar * c
 
 end # module
