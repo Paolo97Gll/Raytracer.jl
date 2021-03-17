34bb0aeeeb98bf977c45f5605aee6414e83caf58
Samuele-Colombo <samuele.colombo1@studenti.unimi.it>
2021-03-17 17:13:21 +0100

    Removed unnecessary module specifications from imported functions and structs

--- Raytracer/src/Raytracer.jl
+++ Raytracer/src/Raytracer.jl
@@ -1,7 +1,11 @@
 module Raytracer
 
 import ColorTypes: RGB
-import Base: Matrix, size, zero
+import Base: Matrix, OneTo
+import Base: (+), (-), (*), (≈) 
+import Base: size, zero, eltype, length, firstindex, lastindex, getindex, getindex, iterate, axes
+import Base.Broadcast: BroadcastStyle
+import Base.Broadcast: broadcastable, copy
 
 include("color.jl")
 include("hdr_image.jl")
--- Raytracer/src/color.jl
+++ Raytracer/src/color.jl
@@ -4,24 +4,24 @@
 
 
 """Dispatch of elementwise addition between two RGB type instances"""
-Base.:+(c1::RGB{T}, c2::RGB{T}) where {T} = RGB{T}(c1.r + c2.r, c1.g + c2.g, c1.b + c2.b)
+(+)(c1::RGB{T}, c2::RGB{T}) where {T} = RGB{T}(c1.r + c2.r, c1.g + c2.g, c1.b + c2.b)
 
 """Dispatch of elementwise subtraction di between two RGB type instances"""
-Base.:-(c1::RGB{T}, c2::RGB{T}) where {T} = RGB{T}(c1.r - c2.r, c1.g - c2.g, c1.b - c2.b)
+(-)(c1::RGB{T}, c2::RGB{T}) where {T} = RGB{T}(c1.r - c2.r, c1.g - c2.g, c1.b - c2.b)
 
 """Dispatch of scalar multiplication for a RGB type instance"""
-Base.:*(scalar::Number, c::RGB{T}) where {T} = RGB{T}(scalar * c.r, scalar * c.g, scalar * c.b)
+(*)(scalar::Number, c::RGB{T}) where {T} = RGB{T}(scalar * c.r, scalar * c.g, scalar * c.b)
 
 """Mirrored version of scalar multiplication for a RGB type instance"""
-Base.:*(c::RGB{T}, scalar::Number) where {T} = scalar * c
+(*)(c::RGB{T}, scalar::Number) where {T} = scalar * c
 
 """Dispatch of elementwise multiplication between two RGB type instances"""
-Base.:*(c1::RGB{T}, c2::RGB{T}) where {T} = RGB{T}(c1.r * c2.r, c1.g * c2.g, c1.b * c2.b)
+(*)(c1::RGB{T}, c2::RGB{T}) where {T} = RGB{T}(c1.r * c2.r, c1.g * c2.g, c1.b * c2.b)
 
 """Dispatch of elementwise ≈ operator between two RGB type instances"""
-Base.:≈(c1::RGB{T}, c2::RGB{T}) where {T} = c1.r ≈ c2.r && 
-                                            c1.g ≈ c2.g &&
-                                            c1.b ≈ c2.b
+(≈)(c1::RGB{T}, c2::RGB{T}) where {T} = c1.r ≈ c2.r && 
+                                        c1.g ≈ c2.g &&
+                                        c1.b ≈ c2.b
 
 
 ############
@@ -30,17 +30,17 @@ Base.:≈(c1::RGB{T}, c2::RGB{T}) where {T} = c1.r ≈ c2.r &&
 
 
 # implementing an iterator over RGB
-Base.eltype(c::RGB{T}) where {T} = T
+eltype(::RGB{T}) where {T} = T
 
-Base.length(c::RGB{T}) where {T} = 3
+length(c::RGB{T}) where {T} = 3
 
-Base.firstindex(c::RGB{T}) where {T} = 1
+firstindex(c::RGB{T}) where {T} = 1
 
-function Base.lastindex(c::RGB{T}) where {T}
+function lastindex(c::RGB{T}) where {T}
     length(c)
 end
 
-function Base.getindex(c::RGB{T}, i::Integer) where {T}
+function getindex(c::RGB{T}, i::Integer) where {T}
     if i == 1
         return c.r
     elseif i == 2
@@ -51,11 +51,11 @@ function Base.getindex(c::RGB{T}, i::Integer) where {T}
         throw(BoundsError(c, i))
     end
 end
-function Base.getindex(c::RGB{T}, i::CartesianIndex{1}) where {T}
-    Base.getindex(c, Tuple(i)[1])
+function getindex(c::RGB{T}, i::CartesianIndex{1}) where {T}
+    getindex(c, Tuple(i)[1])
 end
 
-function Base.iterate(c::RGB{T}, state = 1) where {T}
+function iterate(c::RGB{T}, state = 1) where {T}
     state > 3 ? nothing : (c[state], state +1)
 end
 
@@ -75,26 +75,26 @@ end
 # array-like type. We'll trick the broadcasting process into thinking this is the case for `RGB` so that
 # `broadcastable` will return its argument when it is applied to an `RGB`
 
-Base.axes(::RGB{T}) where {T} = (Base.OneTo(3),) 
+axes(::RGB{T}) where {T} = (OneTo(3),) 
 
-Base.broadcastable(c::RGB{T}) where {T} = c 
-Base.broadcastable(::Type{RGB}) = RGB # broadcastable is also applied to types
+Broadcast.broadcastable(c::RGB{T}) where {T} = c 
+Broadcast.broadcastable(::Type{RGB}) = RGB # broadcastable is also applied to types
 
 # then the result of the previous mapping is passed onto the function `combine_styles`, which
 # relies on calls to the constructor of `BroadcastStyle`. We need to create a `BroadcastStyle` 
 # exclusive to `RGB` so that we can then specialize other methods that are fed `BroadcastStyle`s
 struct RGBBroadcastStyle <: Broadcast.BroadcastStyle end
 # then we specialize the constructor from instances of `RGB`
-Base.BroadcastStyle(::Type{<:RGB}) = RGBBroadcastStyle()
+Broadcast.BroadcastStyle(::Type{<:RGB}) = RGBBroadcastStyle()
 # and the constructor that combines the `RGBBroadcastStyle` with any other `BroadcastStyle`
 # we want our style to have precedence over any other so that if an `RGB` type is present among
 # the arguments of broadcasting the result will be of type `RGB`
-Base.BroadcastStyle(::Raytracer.RGBBroadcastStyle, ::Broadcast.BroadcastStyle) = Raytracer.RGBBroadcastStyle()
+Broadcast.BroadcastStyle(::Raytracer.RGBBroadcastStyle, ::Broadcast.BroadcastStyle) = Raytracer.RGBBroadcastStyle()
 
 # the call to `materialize` returns a call to `copy`, which by default is specialized for array-like types
 # we then have to implement a method that treats any `Broadcasted` type with our custom `RGBBroadcastStyle`
 # in an appropriate way. 
-@inline function Base.Broadcast.copy(bc::Broadcast.Broadcasted{Raytracer.RGBBroadcastStyle})
+@inline function Broadcast.copy(bc::Broadcast.Broadcasted{Raytracer.RGBBroadcastStyle})
     # the call to `convert` to a `Broadcasted` type of style `Nothing` computes
     # the result of the broadcasting and stores it into an array. splatting this
     # array into an `RGB` constructor gives us the desired result
@@ -108,6 +108,6 @@ end
 
 
 function zero(x::Type{RGB})
-    T = eltype(x)
-    RGB(zero(T), zero(T), zero(T))
+    z = zero(eltype(x))
+    RGB(z, z, z)
 end
