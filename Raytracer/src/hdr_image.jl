3a5df8c72feaadb8cfa9259837974f1c0ffbaa33
Samuele-Colombo <samuele.colombo1@studenti.unimi.it>
2021-03-17 18:09:04 +0100

    Implemented broadcasting

--- Raytracer/src/hdr_image.jl
+++ Raytracer/src/hdr_image.jl
@@ -32,4 +32,13 @@ end
 
 # TODO implement broadcasting
 
+axes(image::HdrImage) = axes(image.pixel_matrix)
+
+Broadcast.BroadcastStyle(::Type{<:HdrImage{T}}) where {T} = Broadcast.Style{HdrImage{T}}
+Broadcast.BroadcastStyle(::Broadcast.Style{HdrImage{T}}, ::Broadcast.BroadcastStyle) where {T} = Broadcast.Style{HdrImage{T}}()
+
+function similar(bc::Broadcast.Broadcasted{Broadcast.Style{HdrImage{T}}}, ::Type{T}) where {T}
+    HdrImage{T}(similar(Matrix{T}, axes(bc)))
+end
+
 # TODO implement tests
