f094ce401126f004035e575f4a7bc6b2f67d39a5
Paolo Galli <49845775+Paolo97Gll@users.noreply.github.com>
2021-03-17 10:44:58 +0100

    Uniforming code styles

--- Raytracer/src/Raytracer.jl
+++ Raytracer/src/Raytracer.jl
@@ -2,25 +2,37 @@ module Raytracer
 
 import ColorTypes.RGB
 
+
+##############
+# OPERATIONS #
+##############
+
+
 """Dispatch of elementwise addition between two RGB type instances"""
-Base.:+(c1::RGB{T}, c2::RGB{T}) where {T} = RGB{T}(c1.r+c2.r,c1.g+c2.g,c1.b+c2.b)
+Base.:+(c1::RGB{T}, c2::RGB{T}) where {T} = RGB{T}(c1.r + c2.r, c1.g + c2.g, c1.b + c2.b)
 
 """Dispatch of elementwise subtraction di between two RGB type instances"""
-Base.:-(c1::RGB{T}, c2::RGB{T}) where {T} = RGB{T}(c1.r-c2.r,c1.g-c2.g,c1.b-c2.b)
+Base.:-(c1::RGB{T}, c2::RGB{T}) where {T} = RGB{T}(c1.r - c2.r, c1.g - c2.g, c1.b - c2.b)
 
 """Dispatch of scalar multiplication for a RGB type instance"""
-Base.:*(scalar::Number, c::RGB{T}) where {T} = RGB{T}((scalar .* (c.r, c.g, c.b))...)
+Base.:*(scalar::Number, c::RGB{T}) where {T} = RGB{T}(scalar * c.r, scalar * c.g, scalar * c.b)
 
 """Mirrored version of scalar multiplication for a RGB type instance"""
 Base.:*(c::RGB{T}, scalar::Number) where {T} = scalar * c
 
 """Dispatch of elementwise multiplication between two RGB type instances"""
-Base.:*(c1::RGB{T}, c2::RGB{T}) where {T} = RGB{T}(((c1.r, c1.g, c1.b) .* (c2.r, c2.g, c2.b))...)
+Base.:*(c1::RGB{T}, c2::RGB{T}) where {T} = RGB{T}(c1.r * c2.r, c1.g * c2.g, c1.b * c2.b)
 
 """Dispatch of elementwise ≈ operator between two RGB type instances"""
-Base.:≈(c1::RGB{T}, c2::RGB{T}) where {T} = c1.r ≈ c2.r &&
+Base.:≈(c1::RGB{T}, c2::RGB{T}) where {T} = c1.r ≈ c2.r && 
                                             c1.g ≈ c2.g &&
                                             c1.b ≈ c2.b
+
+
+############
+# ITERATOR #
+############
+
 
 # implementing an iterator over RGB
 Base.eltype(c::RGB{T}) where {T} = T
@@ -51,6 +63,11 @@ end
 function Base.iterate(c::RGB{T}, state = 1) where {T}
     state > 3 ? nothing : (c[state], state +1)
 end
+
+
+################
+# BROADCASTING #
+################
 
 
 # implementing broadcasting
