5c26ccd119ce94e606610c4048324bd3eb693b38
Samuele-Colombo <samuele.colombo1@studenti.unimi.it>
2021-03-17 10:23:00 +0100

    Add iteration and broadcast implementation and testing

--- Raytracer/test/runtests.jl
+++ Raytracer/test/runtests.jl
@@ -28,3 +28,41 @@ import ColorTypes.RGB
     # test elementwise multiplication
     @test c1 * c2 == RGB(1. * .4, 2. * .5, 3. * .6)
 end
+
+@testset "ColorItarable" begin
+    r = 1.
+    g = 2.
+    b = 3.
+    c = RGB(r,g,b)
+
+    # test indexing
+    @test r == c[begin] == c[1]
+    @test g == c[2]
+    @test b == c[end] == c[3]
+    @test_throws BoundsError c[4]
+
+    # test iterability
+    @test all(i == j for (i, j) in zip((r,g,b), c))
+    
+    # test splat operator 
+    cc = RGB(c...)
+    @test cc == c
+end
+
+@testset "ColorBroadcasting" begin
+    c1 = RGB(1.,2.,3.)
+    c2 = RGB(4.,5.,6.)
+    a = 2
+    # testing equivalence to custom defined methods
+    @test c1 .+ c2 == c1 + c2
+    @test c1 .- c2 == c1 - c2
+    @test c1 .* c2 == c1 * c2
+    @test a .* c2 == a * c2
+
+    # broadcasting operators can be applied between any broadcastable type instances
+    @test all(true == el for el in ((.1, .2, .3) .+ c1 .≈ RGB(1.1, 2.2, 3.3)))
+    @test all(true == el for el in ((1., 2., 3.) .== c1))
+
+    # it works for any operator valid for the types of the elements
+    @test all(true == el for el in (c2 ./ c1 .≈ RGB(4., 5 // 2, 2.)))
+end
