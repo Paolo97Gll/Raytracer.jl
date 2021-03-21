6e3078eecf1241e063abbb37c229ac25cf142065
Paolo Galli <49845775+Paolo97Gll@users.noreply.github.com>
2021-03-21 16:49:01 +0100

    Add test_utilities

--- Raytracer/test/runtests.jl
+++ Raytracer/test/runtests.jl
@@ -1,7 +1,9 @@
 using Raytracer
 using Test
 
+using Raytracer: little_endian
 import ColorTypes.RGB
 
 include("test_color.jl")
-include("hdr_image_test.jl")
+include("test_hdr_image.jl")
+include("test_utilities.jl")
