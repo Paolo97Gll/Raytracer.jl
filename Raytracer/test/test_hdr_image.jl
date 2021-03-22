3b3d443e567c7e5a5711bfc47af5b5796acfe936
Paolo Galli <49845775+Paolo97Gll@users.noreply.github.com>
2021-03-22 21:48:09 +0100

    Add iteration tests

--- Raytracer/test/test_hdr_image.jl
+++ Raytracer/test/test_hdr_image.jl
@@ -1,20 +1,47 @@
 @testset "HDR Image" begin
     @testset "Constructors" begin
-        width, height = 3, 2
+        # test custom constructors
+        img_width, img_height = 2, 3
         rgb_zero = zero(RGB{Float32})
         rgb_zeros = [rgb_zero rgb_zero rgb_zero
                      rgb_zero rgb_zero rgb_zero]
+        @test all(HdrImage{RGB{Float32}}(img_width, img_height).pixel_matrix .=== rgb_zeros)
+        @test all(HdrImage(RGB{Float32}, img_width, img_height).pixel_matrix .=== rgb_zeros)
+        @test all(HdrImage(img_width, img_height).pixel_matrix .=== rgb_zeros)
+        @test all(HdrImage(img_width, img_height) .=== HdrImage{RGB{Float32}}(img_width, img_height))
+        @test all(HdrImage(img_width, img_height) .!== HdrImage{RGB{Float64}}(img_width, img_height))
 
-        @test HdrImage{RGB{Float32}}(width, height).pixel_matrix == rgb_zeros
-        @test HdrImage(RGB{Float32}, width, height).pixel_matrix == rgb_zeros
-        @test HdrImage(width, height).pixel_matrix == rgb_zeros
-        @test HdrImage(width, height).pixel_matrix == HdrImage{RGB{Float32}}(width, height).pixel_matrix
-        @test eltype(HdrImage(width, height).pixel_matrix) == eltype(HdrImage{RGB{Float32}}(width, height).pixel_matrix)
-        @test eltype(HdrImage(width, height).pixel_matrix) != eltype(HdrImage{RGB{Float64}}(width, height).pixel_matrix)
+        # test constructor from matrix
+        rgb_pixel_matrix = [RGB(1., 2., 3.) RGB(10., 11., 12.)
+                            RGB(4., 5., 6.) RGB(13., 14., 15.)
+                            RGB(7., 8., 9.) RGB(16., 17., 18.)]
+        @test all(HdrImage(rgb_pixel_matrix).pixel_matrix .=== rgb_pixel_matrix)
     end
 
+
     @testset "Iterations" begin
-        # TODO Paolo: Iterations tests
+        c1 = RGB(1., 2., 3.)
+        c2 = RGB(4., 5., 6.)
+        c3 = RGB(7., 8., 9.)
+        c4 = RGB(10., 11., 12.)
+        c5 = RGB(13., 14., 15.)
+        c6 = RGB(16., 17., 18.)
+        rgb_pixel_matrix = [c1 c4
+                            c2 c5
+                            c3 c6]
+        image = HdrImage(rgb_pixel_matrix)
+
+        # test indexing
+        @test c1 == image[begin] == image[1]
+        @test c2 == image[2]
+        @test c3 == image[3]
+        @test c4 == image[4]
+        @test c5 == image[5]
+        @test c6 == image[end] == image[6]
+        @test_throws BoundsError image[7]
+
+        # test iterability
+        @test all(i == j for (i, j) in zip((c1, c2, c3, c4, c5, c6), image))
     end
 
 
@@ -29,11 +56,13 @@
 
 
     @testset "Other" begin
-        width, height = 3, 2
-        a = HdrImage(fill(RGB(NaN32,NaN32,NaN32), height, width))
-        @test all(HdrImage(width, height) .== fill!(a, zero(RGB{Float32})))
-        a = HdrImage(ones(RGB{Float32}, height, width))
-        @test all(fill!(HdrImage(width, height), one(RGB{Float32})) .== a)
+        img_width, img_height = 3, 2
+
+        a = HdrImage(fill(RGB(NaN32,NaN32,NaN32), img_height, img_width))
+        @test all(HdrImage(img_width, img_height) .== fill!(a, zero(RGB{Float32})))
+        
+        a = HdrImage(ones(RGB{Float32}, img_height, img_width))
+        @test all(fill!(HdrImage(img_width, img_height), one(RGB{Float32})) .== a)
         # TODO Paolo: Other tests
     end
 end
