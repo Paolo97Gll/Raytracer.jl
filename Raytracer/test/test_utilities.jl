31d93418f266b4f8a452426f8d9a998cb4e1ee88
Paolo Galli <49845775+Paolo97Gll@users.noreply.github.com>
2021-03-23 02:10:37 +0100

    Improve tests for little endian and big endian

--- Raytracer/test/test_utilities.jl
+++ Raytracer/test/test_utilities.jl
@@ -1,8 +1,10 @@
 @testset "Other" begin
     # test little_endian
     if (little_endian)
+        # little endian
         @test reinterpret(UInt32, 1.0f0) == 0x3f800000
     else
+        # big endian
         @test reinterpret(UInt32, 1.0f0) == 0x0000803f
     end
 end
