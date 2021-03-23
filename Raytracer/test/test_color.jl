ace58b94d27c95d0965d9d870a98e22843ea9aef
Paolo Galli <49845775+Paolo97Gll@users.noreply.github.com>
2021-03-23 01:58:42 +0100

    Added more tests for each testset

--- Raytracer/test/test_color.jl
+++ Raytracer/test/test_color.jl
@@ -2,13 +2,13 @@
     @testset "Operations" begin
         c1 = RGB(1., 2., 3.)
         c2 = RGB(.4, .5, .6)
-        
+
         # test elementwise addition
         @test c1 + c2 == RGB(1.4, 2.5, 3.6)
-        
+
         # test elementwise subtraction
         @test c1 - c2 == RGB(.6, 1.5, 2.4)
-        
+
         # test scalar multiplication
         a = 2
         @test a * c1 == RGB(2., 4., 6.)
@@ -31,25 +31,32 @@
         r = 1.
         g = 2.
         b = 3.
-        c = RGB{Float32}(r, g, b)
+        c = RGB(r, g, b)
 
         # test indexing properties
-        @test length(c) == 3
-        @test firstindex(c) == 1
-        @test lastindex(c) == 3
+        @test length(c) === 3
+        @test firstindex(c) === 1
+        @test lastindex(c) === 3
 
         # test indexing
-        @test r == c[begin] == c[1]
+        @test r == c[begin]
+        @test r == c[1]
         @test g == c[2]
-        @test b == c[end] == c[3]
+        @test b == c[end]
+        @test b == c[3]
         @test_throws BoundsError c[4]
+
+        @test r == c[CartesianIndex(1)]
+        @test g == c[CartesianIndex(2)]
+        @test b == c[CartesianIndex(3)]
+        @test_throws BoundsError c[CartesianIndex(4)]
 
         # test iterability
         @test all(i == j for (i, j) in zip((r, g, b), c))
-        
+
         # test splat operator 
         cc = RGB(c...)
-        @test cc == c
+        @test cc === c
     end
 
 
@@ -62,7 +69,8 @@
         @test c1 .+ c2 == c1 + c2
         @test c1 .- c2 == c1 - c2
         @test c1 .* c2 == c1 * c2
-        @test a .* c2 == a * c2
+        @test a .* c1 == a * c1
+        @test c1 .* a == c1 * a
 
         # broadcasting operators can be applied between any broadcastable type instances
         @test all(true == el for el in ((.1, .2, .3) .+ c1 .≈ RGB(1.1, 2.2, 3.3)))
@@ -74,31 +82,31 @@
 
 
     @testset "IO" begin
-        c = RGB(1., 2., 3.)
-
-        # test color pretty printing (compact)
-        io = IOBuffer()
-        show(io, c)
-        @test String(take!(io)) == "(1.0 2.0 3.0)"
-        
-        # test color pretty printing (extended)
-        io = IOBuffer()
-        show(io, "text/plain", c)
-        @test String(take!(io)) == "RGB color with eltype Float64\nR: 1.0, G: 2.0, B: 3.0"
-
-        # test color write to IO (Float32)
         io = IOBuffer()
         c_f32 = RGB{Float32}(1., 2., 3.)
-        write(io, c_f32)
-        @test reinterpret(Float32, take!(io)) == [1., 2., 3.]
+        c_f64 = RGB{Float64}(1., 2., 3.)
 
-        # test color write to IO (Float64)
-        io = IOBuffer()
-        write(io, c)
-        @test reinterpret(Float32, take!(io)) == [1., 2., 3.]
-        # test warning
+        # test color pretty printing
+        # compact
+        show(io, c_f64)
+        @test String(take!(io)) == "(1.0 2.0 3.0)"
+        # extended
+        show(io, "text/plain", c_f64)
+        @test String(take!(io)) == "RGB color with eltype Float64\nR: 1.0, G: 2.0, B: 3.0"
+
+        # test color write to IO
+        # Float32
+        write(io, c_f32)
+        readed_value = reinterpret(Float32, take!(io))
+        @test all(readed_value .=== Array{Float32}([1., 2., 3.]))
+        @test RGB(readed_value...) === c_f32
+        # Other type
         warn_message = "Implicit conversion from Float64 to Float32, since PFM images works with 32bit floating point values"
-        @test_logs (:warn, warn_message) write(io, c)
+        @test_logs (:warn, warn_message) write(io, c_f64)
+        readed_value = reinterpret(Float32, take!(io))
+        @test all(readed_value .=== Array{Float32}([1., 2., 3.]))
+        @test RGB(readed_value...) === c_f32
+        @test RGB(readed_value...) !== c_f64
     end
 
 
@@ -106,17 +114,19 @@
         # test eltype
         @test eltype(RGB{Float32}) == Float32
 
-        # test zero from type
+        # test zero
+        # from type
         @test zero(RGB{Float32}) == RGB{Float32}(0., 0., 0.)
         @test zero(RGB{Float64}) == RGB{Float64}(0., 0., 0.)
-        # test zero from variable
+        # from variable
         c = RGB(1., 2., 3.)
         @test zero(c) == RGB(0., 0., 0.)
 
-        # test one from type
+        # test one
+        # from type
         @test one(RGB{Float32}) == RGB{Float32}(1., 1., 1.)
         @test one(RGB{Float64}) == RGB{Float64}(1., 1., 1.)
-        # test one from variable
+        # from variable
         c = RGB(1., 2., 3.)
         @test one(c) == RGB(1., 1., 1.)
     end
