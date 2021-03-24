04cfacc9ca742ba052b1bfa494310e27db1cf4cf
Samuele-Colombo <samuele.colombo1@studenti.unimi.it>
2021-03-24 11:45:08 +0100

    Implement _parse_endianness

--- Raytracer/src/hdr_image.jl
+++ Raytracer/src/hdr_image.jl
@@ -159,6 +159,29 @@ function write(io::IO, image::HdrImage)
         (c for c ∈ image[:, end:-1:begin])...)
 end
 
+function _parse_img_size()
+    
+end
+
+function _parse_endianness(line::String)
+    DestT = Float32
+    endian_spec = try
+        parse(DestT, line)
+    catch e 
+        if isa(e, ArgumentError)
+            throw(InvalidPfmFileFormat("endianness specification is not parsable to type $DestT"))
+        end
+    end
+
+    valid_spec = one(DestT)
+    if endian_spec == valid_spec
+        return ntoh
+    elseif endian_spec == -valid_spec
+        return ltoh
+    else
+        throw(InvalidPfmFileFormat("invalid endianness specification: got $endian_spec needed ±$valid_spec"))
+    end
+end
 
 # #########
 # # OTHER #
