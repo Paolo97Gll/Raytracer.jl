3de27a6ef36c0d24a5242a627ef3ba1ccec2be8f
Samuele-Colombo <samuele.colombo1@studenti.unimi.it>
2021-03-24 18:16:24 +0100

    Add the FE type to aid in overloading the read function (add working examples)

--- Raytracer/src/utilities.jl
+++ Raytracer/src/utilities.jl
@@ -2,3 +2,25 @@
 Store bool about endianness of host system
 """
 const little_endian = ENDIAN_BOM == 0x04030201
+
+# We define a singleton type FE{file encoding symbol} for each file encoding, so
+# that Julia's dispatch and overloading mechanisms can be used to
+# dispatch read!.
+
+"""
+    FE
+A type representing a standard file data encoding. "FE" stands for
+"File Encoding".
+A `FE` object can be passed as the second argument to [`read`](@ref) to
+request the given stream to be decoded from the given encoding.
+# Examples
+```jldoctest
+
+```
+""" # TODO insert example here
+
+struct FE{stream_type} end
+
+macro FE_str(s)
+    :(FE{$(Expr(:quote, Symbol(s)))})
+end
