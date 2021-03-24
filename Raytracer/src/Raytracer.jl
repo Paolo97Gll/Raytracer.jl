5ac522c8708f7650e0dc34d2a152216823cd07d7
Paolo Galli <49845775+Paolo97Gll@users.noreply.github.com>
2021-03-24 10:30:38 +0100

    Add exception InvalidPfmFileFormat

--- Raytracer/src/Raytracer.jl
+++ Raytracer/src/Raytracer.jl
@@ -1,13 +1,15 @@
 module Raytracer
 
-import ColorTypes: RGB, Fractional
+import Base: Exception, showerror
 import Base: Matrix, OneTo, print_matrix
 import Base: (+), (-), (*), (≈)
 import Base: size, zero, one, fill!, eltype
 import Base: length, firstindex, lastindex, getindex, setindex!, iterate, axes, show, write
 import Base.Broadcast: BroadcastStyle, Style, Broadcasted, combine_eltypes
 import Base.Broadcast: broadcastable, copy, similar
+import ColorTypes: RGB, Fractional
 
+include("exceptions.jl")
 include("color.jl")
 include("hdr_image.jl")
 include("utilities.jl")
