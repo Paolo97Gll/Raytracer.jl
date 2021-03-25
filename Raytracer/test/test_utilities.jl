725036868043f838ea71f542a6ac47daaad1fadd
Samuele-Colombo <samuele.colombo1@studenti.unimi.it>
2021-03-25 21:39:55 +0100

    Add test for @FE_str macro

--- Raytracer/test/test_utilities.jl
+++ Raytracer/test/test_utilities.jl
@@ -7,4 +7,12 @@
         # big endian
         @test reinterpret(UInt32, 1.0f0) == 0x0000803f
     end
+
+    #test @FE_str
+    sym = :test
+    str = String(sym)
+    @test FE(str) == FE{sym}() 
+    @test (@FE_str test) == FE"test"
+    @test typeof(FE(str)) == FE"test"
+
 end
