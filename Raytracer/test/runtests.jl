37b9d383042903537f8683eda5133ca15dab8a31
Samuele-Colombo <samuele.colombo1@studenti.unimi.it>
2021-03-29 16:36:34 +0200

    Move doctest to the end of tests for clearer testing error output

--- Raytracer/test/runtests.jl
+++ Raytracer/test/runtests.jl
@@ -4,9 +4,6 @@ using Test, Documenter
 using Raytracer: little_endian, _read_line, _read_type, _parse_endianness, _parse_int, _parse_img_size, _TypeStream, _read_matrix
 
 DocMeta.setdocmeta!(Raytracer, :DocTestSetup, :(using Raytracer); recursive=true)
-@testset "doctest" begin
-    doctest(Raytracer, manual = false)
-end
 
 @testset "Color" begin
     include("test_color.jl")
@@ -18,4 +15,8 @@ end
 
 @testset "Other" begin
     include("test_utilities.jl")
+end
+
+@testset "doctest" begin
+    doctest(Raytracer, manual = false)
 end
