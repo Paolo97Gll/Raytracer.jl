d315cdf0feb97aacb2f811d7c1923658b646329d
Paolo Galli <49845775+Paolo97Gll@users.noreply.github.com>
2021-03-31 13:07:21 +0200

    Add test main

--- main.jl
+++ main.jl
@@ -6,7 +6,15 @@ Pkg.activate(normpath(@__DIR__))
 using Raytracer
 
 function main()
-    println("Hello, world!")
+    image = open("test/memorial.pfm") do io
+        read(io, FE("pfm"))
+    end
+    image = normalize_image(image, 0.5)
+    image = clamp_image(image)
+    save("prova.png", image)
+    # open("prova_w.png", "w") do io
+    #     write(io, FE("PNG"), image)
+    # end
 end
 
 main()
