be1a7787a581c697db0a81e41f6afdb98b744a3e
Samuele-Colombo <samuele.colombo1@studenti.unimi.it>
2021-04-05 01:11:52 +0200

    Fix import/exports layout

--- src/Raytracer.jl
+++ src/Raytracer.jl
@@ -4,17 +4,19 @@ import Base: Exception, showerror
 import Base: Matrix, OneTo, print_matrix
 import Base: (+), (-), (*), (≈)
 import Base: size, zero, one, fill!, eltype
-import Base: length, firstindex, lastindex, getindex, setindex!, iterate, axes, show, write
+import Base: length, firstindex, lastindex, getindex, setindex!, iterate, axes
+import Base: show, write
 import Base: readline, read
 import Base.Broadcast: BroadcastStyle, Style, Broadcasted, combine_eltypes
 import Base.Broadcast: broadcastable, copy, similar
 import ColorTypes: RGB, Fractional
-import ImageIO: DataFormat, Stream, File, save, load
+import ImageIO: save, load
 
 export RGB
 export HdrImage
-export RaytracerException, InvalidPfmFileFormat, InvalidRgbStream
-export normalize_image, clamp_image, save, load
+export RaytracerException, InvalidRgbStream
+export normalize_image, clamp_image, γ_correction
+export save, load
 
 include("exceptions.jl")
 include("color.jl")
