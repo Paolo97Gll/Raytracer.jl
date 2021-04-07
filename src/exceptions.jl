f0ba78c6a6fd3cb3f5094173702fa5ef44d1e712
Paolo Galli <49845775+Paolo97Gll@users.noreply.github.com>
2021-04-07 13:59:01 +0200

    Delete exceptions.jl

--- src/exceptions.jl
+++
@@ -1,6 +0,0 @@
-abstract type RaytracerException <: Exception end
-
-struct InvalidRgbStream <: RaytracerException
-    message::String
-end
-Base.showerror(io::IO, e::InvalidRgbStream) = print(io, typeof(e), ": ", e.message)
