f15b1b8090e76e082c61bdf050666589ce344428
Paolo Galli <49845775+Paolo97Gll@users.noreply.github.com>
2021-04-14 11:54:27 +0200

    Add section separators

--- src/geometry.jl
+++ src/geometry.jl
@@ -7,13 +7,23 @@
 # description:
 #   Implementation of the geometry required for the generation
 #   and manipulation of a 3D scene.
+
+
 abstract type RaytracerGeometry end
+
+
+#####################################################################
+
 
 struct Vec{T<:Real} <: RaytracerGeometry
     x::T
     y::T
     z::T
 end
+
+
+#####################################################################
+
 
 struct Point{T<:Real} <: RaytracerGeometry
     x::T
