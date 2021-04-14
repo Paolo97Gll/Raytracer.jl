2eb024569d6e814e8b668b63d31e4d41448203cd
Paolo Galli <49845775+Paolo97Gll@users.noreply.github.com>
2021-04-14 18:51:22 +0200

    Create test_geometry.jl

---
+++ test/test_geometry.jl
@@ -0,0 +1,8 @@
+# Raytracer.jl
+# Raytracing for the generation of photorealistic images in Julia
+# (C) 2021 Samuele Colombo, Paolo Galli
+#
+# file:
+#   test_geometry.jl
+# description:
+#   Unit test for geometry.jl
