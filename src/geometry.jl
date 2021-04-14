9cf2ee3dbf33c2005e3d23705221403d84442ebe
Paolo Galli <49845775+Paolo97Gll@users.noreply.github.com>
2021-04-14 11:45:41 +0200

    Add header

---
+++ src/geometry.jl
@@ -0,0 +1,9 @@
+# Raytracer.jl
+# Raytracing for the generation of photorealistic images in Julia
+# (C) 2021 Samuele Colombo, Paolo Galli
+#
+# file:
+#   geometry.jl
+# description:
+#   Implementation of the geometry required for the generation
+#   and manipulation of a 3D scene.
