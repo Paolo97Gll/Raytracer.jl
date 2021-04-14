03f3f321d38de43c00be45f394a0f6c89f44fa66
Paolo Galli <49845775+Paolo97Gll@users.noreply.github.com>
2021-04-14 17:03:35 +0200

    First implementation of Point and Vec

--- src/geometry.jl
+++ src/geometry.jl
@@ -15,18 +15,53 @@ abstract type RaytracerGeometry end
 #####################################################################
 
 
-struct Vec{T<:Real} <: RaytracerGeometry
-    x::T
-    y::T
-    z::T
+abstract type VectorSpace{T<:Real} <: RaytracerGeometry end
+
+
+eltype(::VectorSpace{T}) where {T} = T
+eltype(::Type{VectorSpace{T}}) where {T} = T
+
+
+# Show in compact mode (i.e. inside a container)
+function show(io::IO, a::VectorSpace)
+    print(io, "$(typeof(a))($(a.x) $(a.y) $(a.z))")
+end
+
+# Human-readable show (more extended)
+function show(io::IO, ::MIME"text/plain", a::VectorSpace{T}) where {T}
+    print(io, "$(typeof(a)) with eltype $T\n", "x = $(a.x), y = $(a.y), z = $(a.z)")
 end
 
 
 #####################################################################
 
 
-struct Point{T<:Real} <: RaytracerGeometry
+struct Vec{T} <: VectorSpace{T}
     x::T
     y::T
     z::T
 end
+
+
+struct Point{T} <: VectorSpace{T}
+    x::T
+    y::T
+    z::T
+end
+
+
+for T ∈ (:Vec, :Point)
+    quote
+        function (≈)(a1::$T, a2::$T)
+            Base.:≈(a1.x, a2.x) &&
+            Base.:≈(a1.y, a2.y) &&
+            Base.:≈(a1.z, a2.z)
+        end
+    end |> eval
+end
+
+
+(+)(p::V, v::Vec) where {V <: Union{Point, Vec}} = eval(nameof(V))(p.x + v.x, p.y + v.y, p.z + v.z)
+
+(-)(p::V, v::Vec) where {V <: Union{Point, Vec}} = eval(nameof(V))(p.x - v.x, p.y - v.y, p.z - v.z)
+(-)(p1::Point, p2::Point) = Vec(p1.x - p2.x, p1.y - p2.y, p1.z - p2.z)
