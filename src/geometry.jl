76b91d4a548d910deaf65a16a0ef75ae3dad66f1
Paolo Galli <49845775+Paolo97Gll@users.noreply.github.com>
2021-04-21 13:19:42 +0200

    Fix Vec and Point structs and add Normal and Transformation structs
    Remove TypedDelegation in favor of FieldVector, first implementation of Transformation struct.
    
    Co-authored-by: Samuele-Colombo <Samuele-Colombo@users.noreply.github.com>

--- src/geometry.jl
+++ src/geometry.jl
@@ -9,47 +9,90 @@
 #   and manipulation of a 3D scene.
 
 
-abstract type RaytracerGeometry end
+#####################################################################
+
+
+for T ∈ (:Vec, :Normal)
+    quote
+        struct $T{V} <: FieldVector{3, V}
+            x::V
+            y::V
+            z::V
+        end
+
+        # Show in compact mode (i.e. inside a container)
+        function show(io::IO, a::$T)
+            print(io, typeof(a), "(", join((string(el) for el ∈ a), ", "), ")")
+        end
+
+        # Human-readable show (more extended)
+        function show(io::IO, ::MIME"text/plain", a::$T)
+            print(io, $T, " with eltype $(eltype(a))\n", join(("$label = $el" for (label, el) ∈ zip((:x, :y, :z), a)), ", "))
+        end
+
+        norm²(v::$T) = sum(el -> el^2, v)
+    end |> eval
+end
 
 
 #####################################################################
 
 
-abstract type VectorSpace{T<:Real} <: RaytracerGeometry end
-
-
-eltype(::VectorSpace{T}) where {T} = T
-eltype(::Type{VectorSpace{T}}) where {T} = T
-
-
-for T ∈ (:Vec, :Point)
-    quote
-        struct $T{V} <: VectorSpace{V}
-            v::SVector{3, V}
-        end
-
-        # Convenience constructor
-        $T(v::AbstractArray{T}) where {T} = $T(SVector{size(v)...}(v))
-
-        # Human-readable show (more extended)
-        function show(io::IO, ::MIME"text/plain", a::$T)
-            print(io, $T, " with eltype $(eltype(a))\n", join(("$label = $el" for (label, el) ∈ zip((:x, :y, :z), a.v)), ", "))
-        end
-
-        # Show in compact mode (i.e. inside a container)
-        function show(io::IO, a::$T)
-            print(io, typeof(a), "(", join((string(el) for el ∈ a.v), ", "), ")")
-        end
-    end |> eval
+struct Point{T}
+    v::SVector{3, T}
 end
 
-@delegate_onefield(Vec, v, [norm])
-@delegate_onefield_astype(Vec, v, [normalize, (*)])
-norm²(v::Vec) = sum(el -> el^2, v.v)
-@delegate_onefield_twovars(Vec, v, [(≈), (⋅)])
-@delegate_onefield_twovars_astype(Vec, v, [(+), (-), (×)])
-(*)(s, v::Vec) = v * s
+eltype(::Point{T}) where {T} = T
+eltype(::Type{Point{T}}) where {T} = T
 
+# Convenience constructor
+Point(p::AbstractArray{T}) where {T} = Point(SVector{size(p)...}(p))
+
+# Show in compact mode (i.e. inside a container)
+function show(io::IO, a::Point)
+    print(io, typeof(a), "(", join((string(el) for el ∈ a.v), ", "), ")")
+end
+
+# Human-readable show (more extended)
+function show(io::IO, ::MIME"text/plain", a::Point)
+    print(io, Point, " with eltype $(eltype(a))\n", join(("$label = $el" for (label, el) ∈ zip((:x, :y, :z), a.v)), ", "))
+end
+
+(≈)(p1::Point, p2::Point) = p1.v ≈ p2.v
 (-)(p1::Point, p2::Point) = Vec(p1.v - p2.v)
-(+)(p::Point, v::Vec) = Point(p.v + v.v)
-(-)(p::Point, v::Vec) = Point(p.v - v.v)
+(+)(p::Point, v::Vec) = Point(p.v + v)
+(-)(p::Point, v::Vec) = Point(p.v - v)
+(*)(p::Point, s...) = (*)(p.v, s...) |> Point
+
+
+#####################################################################
+
+
+struct Transformation{V}
+    m::AbstractMatrix{V}
+    invm::AbstractMatrix{V} 
+
+    function Transformation{T}(m::AbstractMatrix{T} = Diagonal(ones(T,4)), invm::AbstractMatrix{T} = (m\I(4))) where {T}
+        @assert(size(m)==size(invm)==(4,4))
+        new{T}(m, invm)
+    end
+end
+
+Transformation(m::AbstractMatrix{T}) where {T} = Transformation{T}(m)
+Transformation(m::AbstractMatrix{T}, invm::AbstractMatrix{T}) where {T} = (@assert(m*invm ≈ I(4)); Transformation{T}(m, invm))
+Transformation(m::Matrix{T}, invm::Matrix{T}) where {T} = Transformation(SMatrix{4, 4, T}(m), SMatrix{4, 4, T}(invm))
+
+eltype(::Transformation{T}) where {T} = T
+eltype(::Type{Transformation{T}}) where {T} = T
+
+function show(io::IO, ::MIME"text/plain", a::Transformation)
+    println(io, "4x4 $(typeof(a)):")
+    println(io, "Matrix:");
+    print_matrix(io, a.m);
+    println(io, "\nInverse matrix:");
+    print_matrix(io, a.invm);
+end
+
+isconsistent(t::Transformation) = (t.m * t.invm) ≈ I(4)
+
+# TODO implement all the other functions and operations
