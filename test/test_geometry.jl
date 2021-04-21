a40bb0d44add8f9287e0d9fff874d68ba1b093f3
Paolo Galli <49845775+Paolo97Gll@users.noreply.github.com>
2021-04-21 13:20:13 +0200

    Tests need to be revised

--- test/test_geometry.jl
+++ test/test_geometry.jl
@@ -8,58 +8,63 @@
 #   Unit test for geometry.jl
 
 
-@testset "VectorSpace" begin
-    s = 3
+s = 3
 
-    vv1 = [1., 2., 3.]
-    vv2 = [1.5, 3., 4.5]
-    vv3 = [10., 20., 30.]
+vv1 = [1., 2., 3.]
+vv2 = [1.5, 3., 4.5]
+vv3 = [10., 20., 30.]
 
-    sv1 = SVector{size(vv1)...}(vv1)
-    sv2 = SVector{size(vv2)...}(vv2)
-    sv3 = SVector{size(vv3)...}(vv3)
+sv1 = SVector{size(vv1)...}(vv1)
+sv2 = SVector{size(vv2)...}(vv2)
+sv3 = SVector{size(vv3)...}(vv3)
 
 
-    @testset "Vec" begin
-        v1, v2 = (sv1, vv2) .|> Vec
+# TODO these tests does not work and need to be fixed
 
-        @testset "constructor" begin
-            @test v1.v == sv1
-            @test v2.v == sv2
-        end
 
-        @testset "operations" begin
-            @test norm(v1) == norm(sv1)
-            @test normalize(v1) == Vec(normalize(sv1))
-            @test norm²(v1) ≈ norm(sv1)^2
+@testset "Vec" begin
+    v1, v2, v3 = (sv1, vv2, vv3) .|> Vec
 
-            @test v1 ⋅ v2 == sv1 ⋅ sv2
-
-            @test v1 + v2 == Vec(sv1 + sv2)
-            @test v1 - v2 == Vec(sv1 - sv2)
-            @test v1 × v2 == Vec(sv1 × sv2)
-
-            @test s * v1 == v1 * s == Vec(s * sv1)
-
-            @test Vec([15, 30, 45]) ≈ v2 * 10
-        end
+    @testset "constructor" begin
+        @test v1.v == sv1
+        @test v2.v == sv2
     end
 
+    @testset "operations" begin
+        @test norm(v1) == norm(sv1)
+        @test normalize(v1) == Vec(normalize(sv1))
+        @test norm²(v1) ≈ norm(sv1)^2
 
-    @testset "Point" begin
-        p1, p2 = (sv1, vv2) .|> Point
-        v = Vec(vv3)
+        @test v1 ⋅ v2 == sv1 ⋅ sv2
 
-        @testset "constructor" begin
-            @test p1.v == sv1
-            @test p2.v == sv2
-        end
+        @test v1 + v2 == Vec(sv1 + sv2)
+        @test v1 - v2 == Vec(sv1 - sv2)
+        @test v1 × v2 == Vec(sv1 × sv2)
 
-        @testset "operations" begin
-            @test p1 - p2 == Vec(sv1 - sv2)
+        @test s * v1 == v1 * s == Vec(s * sv1)
 
-            @test p1 + v == Point(sv1 + sv3)
-            @test p1 - v == Point(sv1 - sv3)
-        end
+        @test Vec([15, 30, 45]) ≈ v2 * 10
+    end
+end
+
+
+@testset "Point" begin
+    p1, p2 = (sv1, vv2) .|> Point
+    v = Vec(vv3)
+
+    @testset "constructor" begin
+        @test p1.v == sv1
+        @test p2.v == sv2
+    end
+
+    @testset "operations" begin
+        @test p1 ≈ 
+
+        @test p1 - p2 == Vec(sv1 - sv2)
+
+        @test p1 + v == Point(sv1 + sv3)
+        @test p1 - v == Point(sv1 - sv3)
+
+        @test Point([15, 30, 45]) ≈ p2 * 10
     end
 end
