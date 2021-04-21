faf422fdb11d7541b9dec15a2fd8e01f555d2fa2
Samuele-Colombo <79973069+Samuele-Colombo@users.noreply.github.com>
2021-04-21 23:37:53 +0200

    Add docstrings

--- src/geometry.jl
+++ src/geometry.jl
@@ -34,14 +34,36 @@ for V ∈ (:Vec, :Normal)
     end |> eval
 end
 
+# docstrings
+let docmsg = V ->"""
+        $V{T} <: FieldVector{3, T}
+    
+    A $(V == :Normal ? "pseudo-" : "")vector in 3D space.
+    """
+    @doc docmsg(:Vec)    Vec
+    @doc docmsg(:Normal) Normal
+end
 
 #####################################################################
 
+"""
+    Point{T}
 
+A point in a 3D space. Implemented as a wrapper struct around an `SVector{3, T}`.
+"""
 struct Point{T}
     v::SVector{3, T}
 end
 
+# Convenience constructors
+"""
+    Point(x, y, z)
+    Point(p::AbstractVector)
+
+Construct a `Point` with given coordinates.
+
+If an `AbstractVector` is provided as argument it must have a size = (3,)
+"""
 Point(x::Real, y::Real, z::Real) = Point(SVector(x,y,z))
 function Point(p::AbstractVector)
     size(p) == (3,) || throw(ArgumentError("argument 'p' has size = $(size(p)) but 'Point' requires an argument of size = (3,)"))
@@ -97,6 +119,13 @@ function show(io::IO, ::MIME"text/plain", t::Transformation)
     print_matrix(io, t.invm);
 end
 
+"""
+    isconsistent(t)
+
+Return `true` if `t.m * t.invm` is similar to the identity matrix.
+
+Mainly used for testing and to verify matrices haven't been mutated.
+"""
 isconsistent(t::Transformation) = (t.m * t.invm) ≈ I(4)
 
 
@@ -105,6 +134,20 @@ isconsistent(t::Transformation) = (t.m * t.invm) ≈ I(4)
 (*)(t ::Transformation, n ::Normal)         = transpose(@view(t.invm[1:3,1:3])) * n
 (*)(t ::Transformation, p ::Point)          = t.m * SVector(p.v..., one(eltype(p))) 
 
+"""
+    inverse(t)
+
+Return the inverse `Transformation`.
+
+Returns a `Transformation` which has the `m` and `invm` fields swapped. 
+Note that the returned `Transformation` may have a different eltype with respect of the given one.
+
+#Examples
+```jldoctest
+julia> t = Transformation(Diagonal([1, 2, 3, 1]))
+julia> inverse(t)
+```
+"""
 inverse(t::Transformation) = Transformation(t.invm, t.m)
 
 let rotation_matrices = Dict(
@@ -128,7 +171,48 @@ let rotation_matrices = Dict(
                                                              rotation_matrices[$ax](-θ))
         end |> eval
     end
+
+    # docstrings
+    let docmsg = ax -> """
+            rotation$ax(θ)
+
+        Return a `Transformation` that rotates a 3D vector field of the given angle around the $ax-axis.
+        
+        If an `AbstractVector` is provided as argument it must have a size = (3,)
+        
+        #Examples
+        ```jldoctest
+        julia> rotation$ax(1, 2, 3)
+        
+        ```            
+        
+        ```jldoctest
+        julia> rotation$ax([1, 2, 3])
+        ```
+        """
+        @doc docmsg(:X) rotationX
+        @doc docmsg(:Y) rotationY
+        @doc docmsg(:Z) rotationZ
+    end
 end
+
+"""
+    translation(x, y, z)
+    translation(v)
+
+Return a `Transformation` that translates a 3D vector field of the given coordinates.
+
+If an `AbstractVector` is provided as argument it must have a size = (3,)
+
+#Examples
+```jldoctest
+julia> translation(1, 2, 3)
+```
+
+```jldoctest
+julia> translation([1, 2, 3])
+```
+"""
 function translation(v::AbstractVector)
     size(v) == (3,) || raise(ArgumentError("argument 'v' has size = $(size(v)) but 'translate' requires an argument of size = (3,)")) 
 
@@ -139,6 +223,30 @@ function translation(v::AbstractVector)
     Transformation(mat, mat⁻¹)
 end
 translation(x::Real, y::Real, z::Real) = translation(Vec(x,y,z)) 
+
+"""
+    scaling(x, y, z)
+    scaling(s::Real)
+    scaling(v::AbstractVector)
+
+Return a `Transformation` that scales a 3D vector field of a given factor for each axis.
+
+If a single `Real` is provided as argument then the scaling is considered uniform.
+If an `AbstractVector` is provided as argument it must have a size = (3,)
+
+#Examples
+```jldoctest
+julia> scaling(1, 2, 3)
+```
+
+```jldoctest
+julia> scaling(2)
+```
+
+```jldoctest
+julia> scaling([1, 2, 3])
+```
+"""
 function scaling(x::Real, y::Real, z::Real)
     Transformation(Diagonal(        [x, y, z, true]), 
                    Diagonal(true ./ [x, y, z, true])) # NOTE: the use of true is to avoid unwanted promotions
