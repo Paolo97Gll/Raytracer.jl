d6ee97d19e098d345692d341f5fffdac0a7906c8
Samuele-Colombo <79973069+Samuele-Colombo@users.noreply.github.com>
2021-04-23 01:37:27 +0200

    Implement remaining tests

--- test/test_geometry.jl
+++ test/test_geometry.jl
@@ -18,10 +18,6 @@ sv1 = SVector{size(vv1)...}(vv1)
 sv2 = SVector{size(vv2)...}(vv2)
 sv3 = SVector{size(vv3)...}(vv3)
 
-
-# TODO these tests does not work and need to be fixed
-
-
 @testset "Vec" begin
     v1, v2, v3 = (sv1, vv2, vv3) .|> Vec
 
@@ -94,5 +90,104 @@ end
         @test p1 - v == Point(sv1 - sv3)
 
         @test Point([15, 30, 45]) ≈ p2 * 10
+    end
+end
+
+@testset "Transformation" begin
+    m     = Diagonal([1,2,3,4]) |> Matrix
+    sm    = SMatrix{4,4}(m)
+    invm  = inv(m)
+    sinvm = SMatrix{4,4}(invm)
+
+    @testset "constructor" begin
+        T = Float64
+        def_t = Transformation{T}() 
+        @test def_t.m == def_t.invm == Diagonal(ones(T, 4)) 
+        @test eltype(def_t) <: T
+
+        t1 = Transformation(m)
+        @test t1.m == m && t1.invm == invm
+        @test isconsistent(def_t)
+
+        t1 = Transformation(sm)
+        @test t1.m == m && t1.invm == invm
+        @test typeof(t1.m) <: SMatrix{4, 4} && typeof(t1.invm) <: SMatrix{4, 4} 
+        @test isconsistent(t1)
+
+        t2 = Transformation(m, invm)
+        @test t1.m == m && t1.invm == invm
+        @test isconsistent(t2)
+
+        t2 = Transformation(sm, invm)
+        @test t1.m == m && t1.invm == invm
+        @test typeof(t1.m) <: SMatrix{4, 4} 
+        @test isconsistent(t2)
+
+        t2 = Transformation(m, sinvm)
+        @test t1.m == m && t1.invm == invm
+        @test typeof(t1.invm) <: SMatrix{4, 4} 
+        @test isconsistent(t2)
+
+        t2 = Transformation(sm, sinvm)
+        @test t1.m == m && t1.invm == invm
+        @test typeof(t1.m) <: SMatrix{4, 4} && typeof(t1.invm) <: SMatrix{4, 4} 
+        @test isconsistent(t2)
+    end
+
+    t1 = translation(1, 2, 3)
+    t2 = rotationZ(π/4)
+    vv = [1., 2., 3.]
+    v = Vec(vv...)
+    n = Normal(vv...)
+    p = Point(vv...)
+
+    @testset "operations" begin
+        prod = t1 * t2
+
+        @test isconsistent(prod) 
+        
+        @test prod.m    == t1.m * t2.m
+        @test prod.invm == t2.invm * t1.invm
+
+        @test prod * v == prod.m[1:3,1:3] * v
+        @test prod * n == transpose(prod.invm[1:3,1:3]) * v
+        @test prod * p == prod.m * [v..., 1.]
+    end
+
+    @testset "methods" begin
+        t = t1 * t2 
+        invt = inverse(t)
+        @test t.m == invt.invm && t.invm == invt.m
+
+        θ = π/4
+        rot_mat = Dict(
+            :X => @SMatrix([   1       0      0    0;
+                               0     cos(θ) sin(θ) 0;
+                               0    -sin(θ) cos(θ) 0;
+                               0       0      0    1]),
+            :Y => @SMatrix([ cos(θ)    0   -sin(θ) 0;
+                               0       1      0    0;
+                           sin(θ)    0    cos(θ) 0;
+                               0       0      0    1]),
+            :Z => @SMatrix([ cos(θ) sin(θ)    0    0;
+                           -sin(θ) cos(θ)    0    0;
+                               0      0       1    0;
+                               0      0       0    1])
+        )
+
+        @test rotationX(θ) ≈ Transformation(rot_mat[:X], transpose(rot_mat[:X]))
+        @test rotationY(θ) ≈ Transformation(rot_mat[:Y], transpose(rot_mat[:Y]))
+        @test rotationZ(θ) ≈ Transformation(rot_mat[:Z], transpose(rot_mat[:Z]))
+
+        id = Diagonal(ones(eltype(v), 4)) |> MMatrix{4, 4}
+        id⁻¹ = copy(id)
+        id[end, 1:3]   =  v
+        id⁻¹[end, 1:3] = -v
+        @test translation(v)    ≈ Transformation(id, id⁻¹)
+        @test translation(v...) ≈ Transformation(id, id⁻¹)
+
+        @test scaling(v) ≈ Transformation(Diagonal([v...,true]), Diagonal(true ./ [v..., true]))
+        @test scaling(v...) ≈ Transformation(Diagonal([v...,true]), Diagonal(true ./ [v..., true]))
+        @test scaling(5) ≈ Transformation(Diagonal([5,5,5,1]), Diagonal(inv.([5,5,5,1])))
     end
 end
