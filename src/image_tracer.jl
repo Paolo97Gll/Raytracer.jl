f28c7c4a03df3896dcea55e9f9134d296a6e440c
Paolo Galli <49845775+Paolo97Gll@users.noreply.github.com>
2021-04-28 13:13:46 +0200

    Add header

---
+++ src/image_tracer.jl
@@ -0,0 +1,8 @@
+# Raytracer.jl
+# Raytracing for the generation of photorealistic images in Julia
+# (C) 2021 Samuele Colombo, Paolo Galli
+#
+# file:
+#   image_tracer.jl
+# description:
+#   TODO fill
