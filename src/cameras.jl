1219727da8cf26f0bfc65ed9a74002292430763f
Samuele-Colombo <79973069+Samuele-Colombo@users.noreply.github.com>
2021-05-04 16:16:08 +0200

    Remove bounds check in fire_ray

--- src/cameras.jl
+++ src/cameras.jl
@@ -77,15 +77,11 @@ Parameters `u` and `v` are bound between `0` and `1`:
 Type parameter `T` is passed onto the [`Ray`](@ref) constructor. Default type is `Float32`.
 """
 function fire_ray(camera::OrthogonalCamera, u, v, T::Type{<:AbstractFloat} = Float32)
-    0 <= u <= 1 || throw(ArgumentError("argument `u` must be bound to the range [0, 1]: got $u"))
-    0 <= v <= 1 || throw(ArgumentError("argument `v` must be bound to the range [0, 1]: got $v"))
     origin = Point(-1.0, (1.0 - 2 * u) * camera.aspect_ratio, 2 * v - 1)
     camera.transformation * Ray{T}(origin, VEC_X, tmin = convert(T, 1))
 end
 
 function fire_ray(camera::PerspectiveCamera, u, v, T::Type{<:AbstractFloat} = Float32)
-    0 <= u <= 1 || throw(ArgumentError("argument `u` must be bound to the range [0, 1]: got $u"))
-    0 <= v <= 1 || throw(ArgumentError("argument `v` must be bound to the range [0, 1]: got $v"))
     origin = Point(-camera.screen_distance, 0, 0)
     direction = Vec(camera.screen_distance, (1.0 - 2 * u) * camera.aspect_ratio, 2 * v - 1)
     camera.transformation * Ray{T}(origin, direction, tmin = convert(T, 1))
