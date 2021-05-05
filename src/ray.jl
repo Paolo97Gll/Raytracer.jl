ae269ac1ab2679c910059a6b24ee3ba17cf9dcd5
Paolo Galli <49845775+Paolo97Gll@users.noreply.github.com>
2021-05-05 16:24:29 +0200

    New Ray implementation for consistency of types

--- src/ray.jl
+++ src/ray.jl
@@ -21,13 +21,13 @@ Members:
 - `depth` (`Int`): number of times this ray was reflected/refracted
 """
 struct Ray{T<:AbstractFloat}
-    origin::Point
-    dir::Vec
+    origin::Point{T}
+    dir::Vec{T}
     tmin::T
     tmax::T
     depth::Int
 
-    function Ray(origin::Point, dir::Vec, tmin::T, tmax::T, depth::Int) where {T}
+    function Ray(origin::Point{T}, dir::Vec{T}, tmin::T, tmax::T, depth::Int) where {T}
         tmin < tmax || throw(ArgumentError("`tmin >= tmax` is not allowed: `tmin = $tmin`, `tmax = $tmax`"))
         new{T}(origin, dir, tmin, tmax, depth)
     end
@@ -38,8 +38,23 @@ end
 
 Construct an instance of [`Ray`](@ref) with `tmin`, tmax
 """
-Ray{T}(origin::Point, dir::Vec; tmin::T = 1e-5, tmax::T = typemax(T), depth::Int = 0) where {T <: AbstractFloat} = Ray(origin, dir, tmin, tmax, depth)
+Ray(origin::Point{T}, dir::Vec{T}; tmin::T = 1e-5, tmax::T = typemax(T), depth::Int = 0) where {T <: AbstractFloat} = Ray(origin, dir, tmin, tmax, depth)
 
+function Ray{T}(origin::Point, dir::Vec, tmin::Real, tmax::Real, depth::Int) where {T <: AbstractFloat} 
+    origin = Point(convert.(T, origin.v))
+    dir = Vec{T}(dir)
+    tmin = convert(T, tmin)
+    tmax = convert(T, tmax)
+    Ray(origin, dir, tmin, tmax, depth)
+end
+
+function Ray{T}(origin::Point, dir::Vec; tmin::Real = 1e-5, tmax::Real = typemax(T), depth::Int = 0) where {T <: AbstractFloat} 
+    origin = Point(convert.(T, origin.v))
+    dir = Vec{T}(dir)
+    tmin = convert(T, tmin)
+    tmax = convert(T, tmax)
+    Ray(origin, dir, tmin, tmax, depth)
+end
 
 """
     (r::Ray)(t::Real)
@@ -62,4 +77,4 @@ eltype(::Type{Ray{T}}) where {T} = T
 
 
 (≈)(r1::Ray, r2::Ray) = (r1.origin ≈ r2.origin) && (r1.dir ≈ r2.dir)
-(*)(t::Transformation, r::Ray) = Ray(t*r.origin, t*r.dir, r.tmin, r.tmax, r.depth)
+(*)(t::Transformation, r::Ray) = Ray{eltype(r)}(t*r.origin, t*r.dir, r.tmin, r.tmax, r.depth)
