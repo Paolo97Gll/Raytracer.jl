90c6d533edbeede381e0f1f2f26e2dffe6aea853
Paolo Galli <49845775+Paolo97Gll@users.noreply.github.com>
2021-05-26 01:01:52 +0200

    Add deprecation warning

--- demo/video_demo.jl
+++ demo/video_demo.jl
@@ -1,11 +1,14 @@
 #!/usr/bin/env julia
+
+@warn "DEPRECATED: USE `raytracer_cli.jl demo animation` INSTEAD"
+sleep(10)
+
 using Pkg
 Pkg.activate("..")
 
 using Raytracer
 using FileIO
-
-# TODO try with 4 threads and 8 threads
+using ProgressMeter
 
 function generate_image(θ::Number)
     # num = lpad(repr(θ), 3, '0')
@@ -18,7 +21,8 @@ function generate_image(θ::Number)
         1,
         FlatRenderer,
         1,
-        1)
+        1,
+        disable_output=true)
     rm(filename * ".pfm")
 end
 
@@ -27,11 +31,18 @@ function generate_animation()
     demodir = "demo_imgs"
     isdir(joinpath(curdir,demodir)) || mkdir(demodir)
     cd(demodir)
-    Threads.@threads for θ ∈ 0:10:359
+    θ_list = 0:10:359
+    p = Progress(length(θ_list), dt=1)
+    Threads.@threads for θ ∈ θ_list
         generate_image(θ)
+        next!(p)
     end
     `ffmpeg -r 6 -pattern_type glob -i 'frame_deg_*.jpg' -c:v libx264 test.mp4` |> run
     cd(curdir)
 end
 
+println("-------------------------------")
+println("| Raytracer.jl animation demo |")
+println("-------------------------------")
+return
 generate_animation()
