1f78447415e9c7a8861dd965adeeae78c6880f92
Samuele-Colombo <79973069+Samuele-Colombo@users.noreply.github.com>
2021-05-25 19:02:04 +0200

    Remove testing renderer

--- src/renderers.jl
+++ src/renderers.jl
@@ -18,12 +18,3 @@ end
 function (fr::FlatRenderer)(ray::Ray)
     (hit = ray_intersection(ray, fr.world)) !== nothing ? hit.material.emitted_radiance(hit.surface_point) + hit.material.brdf.pigment(hit.surface_point) : fr.background_color
 end
-
-Base.@kwdef struct URenderer{T <: AbstractFloat} <:Renderer
-    world::World
-    background_color::RGB{T} = zerto(RGB{T})
-end
-
-function (ur::URenderer)(ray::Ray)
-    (hit = ray_intersection(ray, ur.world)) !== nothing ? RGB(clamp.(hit.surface_point, 0, 1)..., false) : ur.background_color
-end
