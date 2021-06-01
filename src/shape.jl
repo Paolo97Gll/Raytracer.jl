2de66db5be4cb81b08b934d498bc70d34eae0063
Paolo Galli <49845775+Paolo97Gll@users.noreply.github.com>
2021-06-01 06:11:53 +0200

    Minor fixes
    Co-authored-by: Samuele-Colombo <Samuele-Colombo@users.noreply.github.com>

--- src/shape.jl
+++ src/shape.jl
@@ -70,7 +70,7 @@ function ray_intersection(ray::Ray, world::World)
     for shape ∈ world
         last_hit = ray_intersection(ray, shape)
         last_hit === nothing && continue
-        if hit === nothing 
+        if hit === nothing
             hit = last_hit
         else
             last_hit.t/norm(last_hit.ray.dir) >= hit.t/norm(hit.ray.dir) && continue
