3b02eba66d74480c86cabae00418a0dc14eaf92f
Paolo Galli <49845775+Paolo97Gll@users.noreply.github.com>
2021-06-04 02:21:24 +0200

    Rename and split into two files

--- src/shapes.jl
+++ src/shapes.jl
@@ -1,14 +1,13 @@
 # Raytracer.jl
 # Raytracing for the generation of photorealistic images in Julia
-# (C) 2021 Samuele Colombo, Paolo Galli
-#
-# file:
-#   shape.jl
-# description:
-#   Implementation of the abstract type Shape and the derivative
-#   concrete types, such as Sphere
+# Copyright (c) 2021 Samuele Colombo, Paolo Galli
 
-# TODO check docstrings
+# Implementation of the abstract type Shape and the derivative concrete types,
+# such as Sphere or Plane
+# TODO write docstrings
+
+
+################################################################
 
 
 """
@@ -17,12 +16,12 @@
 A struct representing the result of an intersection between
 a [`Ray`](@ref) and a [`Shape`](@ref).
 """
-struct HitRecord{T}
-    world_point::Point{T}
-    normal::Normal{T}
-    surface_point::Vec2D{T}
-    t::T
-    ray::Ray{T}
+struct HitRecord
+    world_point::Point
+    normal::Normal
+    surface_point::Vec2D
+    t::Float32
+    ray::Ray
     material::Material
 end
 
@@ -46,6 +45,7 @@ function (≈)(hr1::HitRecord, hr2::HitRecord)
     hr1.ray             ≈  hr2.ray
 end
 
+
 ################################################################
 
 
@@ -66,24 +66,9 @@ function show(io::IO, ::MIME"text/plain", s::T) where {T <: Shape}
     end
 end
 
-const World = Vector{Shape}
 
-function ray_intersection(ray::Ray, world::World)
-    hit = nothing
-    for shape ∈ world
-        last_hit = ray_intersection(ray, shape)
-        last_hit === nothing && continue
-        if hit === nothing
-            hit = last_hit
-        else
-            last_hit.t/norm(last_hit.ray.dir) >= hit.t/norm(hit.ray.dir) && continue
-            hit = last_hit
-        end
-    end
-    hit
-end
-
-#####################################################################
+#########
+# Sphere
 
 
 """
@@ -92,26 +77,25 @@ end
 A type representing a sphere.
 """
 Base.@kwdef struct Sphere <: Shape
-    transformation::Transformation = Transformation{Bool}()
+    transformation::Transformation = Transformation()
     material::Material = Material()
 end
 
-@doc """
+"""
     ray_intersection(ray, s)
 
 Return an [`HitRecord`](@ref) of the nearest ray intersection with the given [`Shape`](@ref),
 if none exists, return `nothing`.
-""" ray_intersection
-
-function ray_intersection(ray::Ray{T}, s::Sphere) where {T}
+"""
+function ray_intersection(ray::Ray, s::Sphere)
     inv_ray = inv(s.transformation) * ray
     # compute intersection
     origin_vec = convert(Vec, inv_ray.origin)
     a = norm²(inv_ray.dir)
     b = 2f0 * origin_vec ⋅ inv_ray.dir
     c = norm²(origin_vec) - 1f0
-    Δ = b^2 - 4 * a * c
-    Δ < 0 && return nothing
+    Δ = b^2 - 4f0 * a * c
+    Δ < 0f0 && return nothing
     sqrt_Δ = sqrt(Δ)
     t_1 = (-b - sqrt_Δ) / (2f0 * a)
     t_2 = (-b + sqrt_Δ) / (2f0 * a)
@@ -129,11 +113,28 @@ function ray_intersection(ray::Ray{T}, s::Sphere) where {T}
     normal = convert(Normal, hit_point)
     normal = s.transformation * ((normal ⋅ ray.dir < 0f0) ? normal : -normal)
     u = atan(hit_point[2], hit_point[1]) / (2f0 * π)
-    u = u >= 0 ? u : u+1f0
+    u = u >= 0f0 ? u : u+1f0
     v = acos(hit_point[3]) / π
-    surface_point = Vec2D{T}(u, v)
-    HitRecord{T}(world_point, normal, surface_point, hit_t, ray, s.material)
+    surface_point = Vec2D(u, v)
+    HitRecord(world_point, normal, surface_point, hit_t, ray, s.material)
 end
+
+function quick_ray_intersection(ray::Ray, s::Sphere)
+    inv_ray = inv(s.transformation) * ray
+    origin_vec = convert(Vec, inv_ray.origin)
+    a = norm²(inv_ray.dir)
+    b = 2f0 * origin_vec ⋅ inv_ray.dir
+    c = norm²(origin_vec) - 1f0
+    Δ = b^2 - 4f0 * a * c
+    Δ < 0f0 && return false
+    sqrt_Δ = sqrt(Δ)
+    (inv_ray.tmin < (-b-sqrt_Δ)/(2f0*a) < inv_ray.tmax) || (inv_ray.tmin < (-b+sqrt_Δ)/(2f0*a) < inv_ray.tmax)
+end
+
+
+########
+# Plane
+
 
 """
     Plane <: Shape
@@ -141,30 +142,41 @@ end
 A type representing an infinite plane.
 """
 Base.@kwdef struct Plane <: Shape
-    transformation::Transformation = Transformation{Bool}()
+    transformation::Transformation = Transformation()
     material::Material = Material()
 end
 
-function ray_intersection(ray::Ray{T}, s::Plane) where {T}
+function ray_intersection(ray::Ray, s::Plane)
     inv_ray = inv(s.transformation) * ray
-    dz = inv_ray.dir.z
-    t = -inv_ray.origin.v[3]/dz
+    abs(inv_ray.dir.z) < 1f-5 && return nothing
+    t = -inv_ray.origin.v[3] / inv_ray.dir.z
     inv_ray.tmin < t < inv_ray.tmax || return nothing
     hit_point = inv_ray(t)
     world_point = s.transformation * hit_point
-    normal = -sign(dz) * normal_z(T)
-    surface_point = hit_point.v[1:2] - floor.(hit_point.v[1:2]) |> Vec2D{T}
-    HitRecord{T}(world_point, normal, surface_point, t, ray, s.material)
+    normal = -sign(inv_ray.dir.z) * NORMAL_Z
+    surface_point = hit_point.v[1:2] - floor.(hit_point.v[1:2]) |> Vec2D
+    HitRecord(world_point, normal, surface_point, t, ray, s.material)
 end
+
+function quick_ray_intersection(ray::Ray, s::Plane)
+    inv_ray = inv(s.transformation) * ray
+    inv_ray.dir.z < 1f-5 && return false
+    inv_ray.tmin < (-inv_ray.origin.v[3] / inv_ray.dir.z) < inv_ray.tmax
+end
+
+
+#######
+# AABB
+
 
 """
     AABB
 
 A type representing an Axis-Aligned Bounding Box
 """
-Base.@kwdef struct AABB{T}
-    p_M::Point{T} = Point( one(T),  one(T),  one(T)) 
-    p_m::Point{T} = Point(zero(T), zero(T), zero(T))
+Base.@kwdef struct AABB
+    p_M::Point = Point(1f0, 1f0, 1f0) 
+    p_m::Point = Point(0f0, 0f0, 0f0)
 end
 
 """
@@ -172,9 +184,9 @@ end
 
 Return the parameter `t` at which `ray` first hits the bounding box. If no hit exists, return `typemax(eltype(ray))`.
 """
-function ray_intersection(ray::Ray{T}, aabb::AABB) where T
+function ray_intersection(ray::Ray, aabb::AABB)
     dir = ray.dir
     overlap = reduce(intersect, map(t -> Interval(t...), zip(-aabb.p_m.v ./ dir, -aabb.p_M.v ./ dir))) 
-    isempty(overlap) && return typemax(T)
+    isempty(overlap) && return typemax(Float32)
     t = overlap.first
 end
