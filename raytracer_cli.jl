5b19ad9d009b6bade183a9dbc96592fb78e20b27
Paolo Galli <49845775+Paolo97Gll@users.noreply.github.com>
2021-06-05 16:26:23 +0200

    Fix default of delta_theta

--- raytracer_cli.jl
+++ raytracer_cli.jl
@@ -199,7 +199,7 @@ function parse_commandline()
         "--delta_theta", "-D"
             help = "Δθ in camera orientation (around z axis) between each frame; the number of frames generated is [360/Δθ]"
             arg_type = Float32
-            default = 10
+            default = 10f0
         "--fps", "-f"
             help = "FPS (frame-per-second) of the output video"
             arg_type = Int
