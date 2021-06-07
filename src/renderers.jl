5ba37514af4e01a32894fccea210c195299f1d4e
Samuele-Colombo <79973069+Samuele-Colombo@users.noreply.github.com>
2021-06-07 11:49:30 +0200

    Add Renderers docstrings

--- src/renderers.jl
+++ src/renderers.jl
@@ -5,14 +5,27 @@
 # Renderer implementations
 # TODO write docstrings
 
+"""
+    Renderer
 
+Abstract type for functors that map [`Ray`](@ref) to [`RGB`](@ref).
+
+Each subtype of this type must be callable like `(r::Renderer)(ray::Ray)` and must return a `RGB{Float32}`.
+Each subtype of this type sould have a field `world` to check for intersections of the given [`Ray`](@ref).
+"""
 abstract type Renderer end
 
 
 ################
 # OnOffRenderer
 
+"""
+    OnOffRenderer <: Renderer
 
+A basic bichrome renderer that checks whether a [`Ray`](@ref) has collided or not.
+
+This renderer returns its field `off_color` when the given [`Ray`](@ref) is `nothing`, else it returns its field `on_color`.
+"""
 struct OnOffRenderer <: Renderer 
     world::World
     on_color::RGB{Float32}
@@ -29,7 +42,15 @@ end
 ###############
 # FlatRenderer
 
+"""
+    FlatRenderer <: Renderer
 
+A basic renderer that returns the color of the [`Shape`](@ref) first hit by a given [`Ray`](@ref).
+
+This renderer returns the color stored in the `material` field of the [`Shape`](@ref) first hit by the given [`Ray`](@ref) at the hit point.
+To this renderer there is no difference between radiated light and reflected color. There are no shades, diffusions or reflections.
+If there are no hits this renderer returns the value of its field `background_color`, which is `BLACK` by default.
+"""
 struct FlatRenderer <: Renderer
     world::World
     background_color::RGB{Float32}
@@ -46,7 +67,18 @@ end
 #############
 # PathTracer
 
+"""
+    PathTracer <: Renderer
 
+A path-tracing algorithm that considers the optical path from the observer to a light source.
+
+Its fields are:
+- a `background_color` field, storing the value to return if the given [`Ray`](@ref) doesn't hit anything, 
+- a [`PCG`](@ref) random number generator to appropriately scatter rays, 
+- an 'n` field indicating how many scattered rays should be generated,
+- a `max_depth` field indicating the maximum number of scatters a ray should be subjected to before stopping, 
+- a `roulette_depth` field indicating the depth at which the russian roulette algorithm should start (if > 'max_depth` then it will never start).
+"""
 struct PathTracer <: Renderer
     world::World
     background_color::RGB{Float32}
