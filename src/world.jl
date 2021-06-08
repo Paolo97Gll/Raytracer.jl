453e37ba0dac3380792f6661780c75700f1e7f6d
Samuele-Colombo <79973069+Samuele-Colombo@users.noreply.github.com>
2021-06-08 12:30:56 +0200

    Fix is_point_visible

--- src/world.jl
+++ src/world.jl
@@ -27,7 +27,7 @@ function is_point_visible(world::World, point::Point, observer_pos::Point)
     direction = point - observer_pos
     dir_norm = norm(direction)
 
-    ray = Ray(observer_pos, direction, 1f-2 / dir_norm, 1f0)
+    ray = Ray(observer_pos, direction, 1f-2 / dir_norm, 1f0, 0)
 
     return !any(shape -> quick_ray_intersection(ray, shape), world)
 end
