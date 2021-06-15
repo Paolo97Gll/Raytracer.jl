43d5ee09a13efde1ca39364924d097f7f0a8fee1
Samuele-Colombo <79973069+Samuele-Colombo@users.noreply.github.com>
2021-06-15 16:01:53 +0200

    Fix Cone

--- src/shapes/cone.jl
+++ src/shapes/cone.jl
@@ -6,7 +6,6 @@
 #######
 # Cone
 
-# BUG this Cone is bugged in pointlight renderer
 """
     struct Cone{radius_ratio} <: SimpleShape
 
@@ -23,6 +22,7 @@ struct Cone{radius_ratio} <: SimpleShape
     transformation::Transformation
     material::Material
 	function Cone(transformation::Transformation, material::Material, radius_ratio::Float32)
+		abs(radius_ratio) <= 1f0 || throw(ArgumentError("the `radius_ratio` must have an absolute value less than 1: got $radius_ratio"))
 		new{radius_ratio}(transformation, material)
 	end
 end
@@ -55,49 +55,16 @@ Constructor for a [`Cone`](@ref) instance.
 """ Cone(; ::Transformation, ::Material)
 
 function get_t(::Type{Cone{RR}}, ray::Ray) where {RR}
-	tray = RR > 1 ? scaling(-1) * ray : ray
-	â  = Normal{true}(0, 0, 1f0)
-	dx, dy, dz = s⃗ = tray.dir
-	ox, oy, oz = e⃗ = convert(Vec, tray.origin)
-	R₀ = 1f0
-	R₁ = RR -1f0
-	Z₀ = e⃗ ⋅ â
-	Z₁ = s⃗ ⋅ â
-
-	P⃗  = s⃗ - Z₁ * â
-	Q⃗  = e⃗ - Z₀ * â
-
-	B₂ = norm²(P⃗) - R₁^2 * Z₁^2
-	B₁ = Z₁ * R₁ * (R₁ * Z₀ + R₀) - P⃗ ⋅ Q⃗
-	B₀ = norm²(Q⃗) - (R₁ * Z₀ + R₀)^2
-
-	if B₂ ≈ 0f0 && !(B₁ ≈ 0f0)
-		t = B₀/2B₁
-		ray.tmin < t < ray.tmax && 0 <= Z₁ * t + Z₀ <= 1 && return t
-	elseif (Δ = B₁^2 - B₀ * B₂) >= 0f0 
-		t = min((B₁ + sqrt(Δ))/B₂, (B₁ - sqrt(Δ))/B₂)
-		ray.tmin < t < ray.tmax && 0 <= Z₁ * t + Z₀ <= 1 && return t
-	end
-
-    # check if caps are hit
-    tz1, tz2 = (( 1f0 - oz) / dz, - oz / dz)
-    
-	if tz1 < tz2
-		ray.tmin < tz1 < ray.tmax && (ox + tz1 * dx)^2 + (oy + tz1 * dy)^2 <=  RR && return tz1
-		ray.tmin < tz2 < ray.tmax && (ox + tz2 * dx)^2 + (oy + tz2 * dy)^2 <= 1f0 && return tz2
-	else
-		ray.tmin < tz2 < ray.tmax && (ox + tz2 * dx)^2 + (oy + tz2 * dy)^2 <= 1f0 && return tz2
-		ray.tmin < tz1 < ray.tmax && (ox + tz1 * dx)^2 + (oy + tz1 * dy)^2 <=  RR && return tz1
-	end
-    return Inf32
+	res = filter(t -> ray.tmin < t < ray.tmax, get_all_ts(Cone{RR}, ray))
+	isempty(res) && return Inf32
+	minimum(res)
 end
 
 function get_all_ts(::Type{Cone{RR}}, ray::Ray) where {RR}
-	tray = RR > 1 ? scaling(-1) * ray : ray
     res = Vector{Float32}()
 	â  = Normal{true}(0, 0, 1f0)
-	dx, dy, dz = s⃗ = tray.dir
-	ox, oy, oz = e⃗ = convert(Vec, tray.origin)
+	dx, dy, dz = s⃗ = ray.dir
+	ox, oy, oz = e⃗ = convert(Vec, ray.origin)
 	R₀ = 1f0
 	R₁ = RR - 1f0
 	Z₀ = e⃗ ⋅ â
@@ -119,36 +86,36 @@ function get_all_ts(::Type{Cone{RR}}, ray::Ray) where {RR}
 		0 <= Z₁ * t1 + Z₀ <= 1 && push!(res, t1)
 		0 <= Z₁ * t2 + Z₀ <= 1 && push!(res, t2)
 	end
-	length(res) == 2 && return res
 
     # check if caps are hit
     tz1, tz2 = (( 1f0 - oz) / dz, - oz / dz)
     
-	if tz1 < tz2
-		(ox + tz1 * dx)^2 + (oy + tz1 * dy)^2 <=  RR && push!(res, tz1)
-		length(res) == 2 && return res
-		(ox + tz2 * dx)^2 + (oy + tz2 * dy)^2 <= 1f0 && push!(res, tz2)
-		@assert length(res) == 2
-		return res
-	else
-		(ox + tz2 * dx)^2 + (oy + tz2 * dy)^2 <= 1f0 && push!(res, tz2)
-		length(res) == 2 && return res
-		(ox + tz1 * dx)^2 + (oy + tz1 * dy)^2 <=  RR && push!(res, tz1)
-		@assert length(res) == 2
-		return res
+	if !iseven(length(res))
+		(ox + tz1 * dx)^2 + (oy + tz1 * dy)^2 <= RR^2 ? push!(res, tz1) : push!(res, tz2)
+		# @assert (ox + tz2 * dx)^2 + (oy + tz2 * dy)^2 <=  1f0
+	elseif (ox + tz2 * dx)^2 + (oy + tz2 * dy)^2 <=  1f0
+		append!(res, [tz1, tz2])
 	end
-    return Inf32
+	# (ox + tz1 * dx)^2 + (oy + tz1 * dy)^2 <= RR^2 && push!(res, tz1)
+	# (ox + tz2 * dx)^2 + (oy + tz2 * dy)^2 <=  1f0 && push!(res, tz2)
+	@assert iseven(length(res)) "$res\n$ray"
+	return res
 end
 
 function get_normal(::Type{Cone{RR}}, point::Point, ray::Ray) where {RR}
-    z = point.v[3]
+    # z = point.v[3]
+    # # if it comes from the upper cap 
+    # (abs(z) ≈ 1f0) && return -sign(ray.dir.z) * NORMAL_Z
+	# # if it comes from the base
+	# (abs(z) ≈ 0f0) && return  sign(ray.dir.z) * NORMAL_Z
+    z = ray.origin[3]
+	abs_z = abs(z)
     # if it comes from the upper cap 
-    (abs(z) ≈ 1f0) && return -sign(ray.dir.z) * NORMAL_Z
-	# if it comes from the base
-	(abs(z) ≈ 0f0) && return  sign(ray.dir.z) * NORMAL_Z
+	# or if it comes from the base
+    (abs_z ≈ 1f0 || abs_z ≈ 0f0) && return -sign(z) * sign(ray.dir.z) * NORMAL_Z
 
     # if it comes from the side
-	z = atan(1f0 - RR)
+	z = 1/(1f0 - RR) * sign(1/(1 - RR) - point[3])
     x, y = point.v[1:2]
 	normal = Normal(x, y, z) |> normalize
     (normal ⋅ ray.dir < 0) ? normal : -normal
