91b12f8841220f2c740422d14f379dd688c4a95d
Paolo Galli <49845775+Paolo97Gll@users.noreply.github.com>
2021-06-30 15:03:43 +0200

    Remove demo and add new tonemapping overload

--- src/user_utils.jl
+++ src/user_utils.jl
@@ -27,10 +27,11 @@ If `disable_output` is `true`, no message is printed.
 """
 function tonemapping(; input_file::String,
                        output_file::String,
-                       α::Float32,
-                       γ::Float32,
+                       α::Float32 = 0.5f0,
+                       γ::Float32 = 1f0,
                        luminosity::Union{Float32, Nothing} = nothing,
                        disable_output::Bool = false)
+    @assert typeof(query(input_file))<:File{format"PFM"}
     io = disable_output ? devnull : stdout
     println(io, "\n-> TONE MAPPING PROCESS")
     print(io, "Loading input file '$(input_file)'...")
@@ -41,6 +42,23 @@ function tonemapping(; input_file::String,
     print(io, " done!\nSaving final image to '$(output_file)'...")
     save(output_file, image.pixel_matrix)
     println(io, " done!")
+end
+
+"""
+    function tonemapping(input_file::String,
+                         output_file::String,
+                         ; α::Float32 = 0.5f0,
+                           γ::Float32 = 1f0,
+                           luminosity::Union{Float32, Nothing} = nothing,
+                           disable_output::Bool = false)
+"""
+function tonemapping(input_file::String,
+                     output_file::String,
+                     ; α::Float32 = 0.5f0,
+                       γ::Float32 = 1f0,
+                       luminosity::Union{Float32, Nothing} = nothing,
+                       disable_output::Bool = false)
+    tonemapping(input_file=input_file, output_file=output_file, α=α, γ=γ, luminosity=luminosity, disable_output=disable_output)
 end
 
 
@@ -54,15 +72,16 @@ end
              use_threads::Bool = true,
              disable_output::Bool = false)
 
-Render and image given an [`ImageTracer`](@ref) and a [`Renderer`](@ref), and save the output in `output_file`.
+Render an image given an [`ImageTracer`](@ref) and a [`Renderer`](@ref), and save the generated hdr image in `output_file`.
 
 If `use_threads` is `true`, use macro `Threads.@threads`. If `disable_output` is `true`, no message is printed.
 """
 function render(; image_tracer::ImageTracer,
                   renderer::Renderer,
-                  output_file::String,
+                  output_file::String = "out.pfm",
                   use_threads::Bool = true,
                   disable_output::Bool = false)
+    @assert typeof(query(output_file))<:File{format"PFM"}
     io = disable_output ? devnull : stdout
     println(io, "\n-> RENDERING")
     println(io, "Rendering image...")
@@ -74,167 +93,16 @@ end
 
 
 """
-    demo_load_scene(renderer_type::Type{<:Renderer},
-                    pt_n::Int,
-                    pt_max_depth::Int,
-                    pt_roulette_depth::Int)
+    function render_scene_from_script(input_script::String,
+                                      ; output_file::String = "out.jpg",
+                                        α::Float32 = 0.75f0,
+                                        γ::Float32 = 1f0,
+                                        luminosity::Union{Float32, Nothing} = nothing,
+                                        use_threads::Bool = true,
+                                        disable_output::Bool = false)
 
-Private function to create a renderer for the [`demo`](@ref) function.
+Render and image given a SceneLang script, and save the output in `output_file`.
 """
-function demo_load_scene(renderer_type::Type{<:Renderer},
-                         pt_n::Int,
-                         pt_max_depth::Int,
-                         pt_roulette_depth::Int)
-    world = World()
-    if renderer_type <: OnOffRenderer
-        coords_list = map(i -> map(bit -> Bool(bit) ? 1 : -1 , digits(i, base=2, pad=3)) |> collect, 0x00:(0x02^3-0x01))
-        append!(coords_list, [[0f0, 0f0, -1f0], [0f0, 1f0, 0f0]])
-        cube_vertices = [Sphere(transformation = translation(coords * 1.1f0) * scaling(0.22)) for coords ∈ coords_list]
-        append!(world, cube_vertices)
-        return OnOffRenderer(world)
-    elseif renderer_type <: FlatRenderer
-        coords_list = map(i -> map(bit -> Bool(bit) ? 1 : -1 , digits(i, base=2, pad=3)) |> collect, 0x00:(0x02^3-0x01))
-        translation_factor, scaling_factor = 1.1f0, scaling(0.22)
-        cube_vertices = [Sphere(transformation = translation(coords * translation_factor) * scaling_factor,
-                                material = Material(brdf = DiffuseBRDF(pigment = UniformPigment(CYAN))))
-                         for coords ∈ coords_list]
-        other_spheres = [Sphere(transformation = translation([0f0, 1f0, 0f0] * translation_factor) * scaling_factor,
-                                material = Material(brdf = DiffuseBRDF(pigment = CheckeredPigment{8}(color_on = RED, color_off = GREEN)))),
-                         Sphere(transformation = translation([0f0, 0f0, -1f0] * translation_factor) * scaling_factor,
-                                material = Material(brdf = DiffuseBRDF(pigment = ImagePigment(HdrImage([RED GREEN BLUE MAGENTA])))))]
-        ground = [Plane(transformation = translation([0f0, 0f0, -2f0]),
-                        material = Material(brdf = DiffuseBRDF(pigment =CheckeredPigment{4}())))]
-        sky = [Sphere(transformation = translation([0f0, 0f0, 0f0]) * scaling(100),
-                      material = Material(brdf = DiffuseBRDF(pigment = UniformPigment(WHITE))))]
-        append!(world, cube_vertices, other_spheres, ground, sky)
-        return FlatRenderer(world)
-    elseif renderer_type <: PathTracer
-        ground = [Plane(transformation = translation([0f0, 0f0, -1f0]),
-                        material = Material(brdf = DiffuseBRDF(pigment = CheckeredPigment{6}(color_on = RGB(0.3f0, 0.5f0, 0.1f0), color_off = RGB(0.1f0, 0.2f0, 0.5f0)))))]
-        sky = [Sphere(transformation = translation([0f0, 0f0, 0f0]) * scaling(100),
-                      material = Material(brdf = DiffuseBRDF(pigment = UniformPigment(BLACK)),
-                                          emitted_radiance = UniformPigment(WHITE)))]
-        other_spheres = [Sphere(transformation = translation([0.5f0, 0.7f0, 0.1f0]),
-                                material = Material(brdf = DiffuseBRDF(pigment = UniformPigment(RGB(0.2f0, 0.7f0, 0.8f0))))),
-                         Sphere(transformation = translation([-0.2f0, -0.8f0, -0.8f0]) * scaling(0.5f0),
-                                material = Material(brdf = SpecularBRDF(pigment = UniformPigment(RGB(0.6f0, 0.2f0, 0.3f0)))))]
-        append!(world, ground, sky, other_spheres)
-        return PathTracer(world, n = pt_n, max_depth=pt_max_depth, roulette_depth = pt_roulette_depth)
-    elseif renderer_type <: PointLightRenderer
-        ground = [Plane(transformation = translation([0f0, 0f0, -1f0]),
-                        material = Material(brdf = DiffuseBRDF(pigment = CheckeredPigment{6}(color_on = RGB(0.3f0, 0.5f0, 0.1f0), color_off = RGB(0.1f0, 0.2f0, 0.5f0)))))]
-        sky = [Sphere(transformation = translation([0f0, 0f0, 0f0]) * scaling(100),
-                      material = Material(brdf = DiffuseBRDF(pigment = UniformPigment(WHITE * 1f-2))))]
-        other_spheres = [Sphere(transformation = translation([0.5f0, 0.7f0, 0.1f0]),
-                                material = Material(brdf = DiffuseBRDF(pigment = UniformPigment(RGB(0.2f0, 0.7f0, 0.8f0))))),
-                         Sphere(transformation = translation([-0.2f0, -0.8f0, -0.8f0]) * scaling(0.5f0),
-                                material = Material(brdf = DiffuseBRDF(pigment = UniformPigment(RGB(0.6f0, 0.2f0, 0.3f0)))))]
-        append!(world, ground, sky, other_spheres)
-        lights = Lights([PointLight(position=Point(0, 0, 10f0))])
-        return PointLightRenderer(world, lights, BLACK, WHITE * 1f-3)
-    else
-        error("`Renderer` subtype $renderer_type is not supported by this function.")
-    end
-end
-
-"""
-    demo_load_tracer(image_resolution::Tuple{Int, Int},
-                     camera_type::Type{<:Camera},
-                     camera_position::Tuple{Float32, Float32, Float32},
-                     camera_orientation::Tuple{Float32, Float32, Float32},
-                     screen_distance::Float32,
-                     samples_per_side::Int
-                     ; disable_output::Bool = false)
-
-Private function to create a tracer for the [`demo`](@ref) function.
-"""
-function demo_load_tracer(image_resolution::Tuple{Int, Int},
-                          camera_type::Type{<:Camera},
-                          camera_position::Tuple{Float32, Float32, Float32},
-                          camera_orientation::Tuple{Float32, Float32, Float32},
-                          screen_distance::Float32,
-                          samples_per_side::Int
-                          ; disable_output::Bool = false)
-    io = disable_output ? devnull : stdout
-    print(io, "Loading tracing informations...")
-    image = HdrImage(image_resolution...)
-    angx, angy, angz = deg2rad.(camera_orientation)
-    transformation = (rotationX(angx) * rotationY(angy) * rotationZ(angz)) * translation(camera_position...)
-    if camera_type <: PerspectiveCamera
-        camera = PerspectiveCamera(//(image_resolution...), transformation, screen_distance)
-    elseif camera_type <: OrthogonalCamera
-        camera = OrthogonalCamera(//(image_resolution...), transformation)
-    else
-        error("`Camera` subtype $camera_type is not supported by this function.")
-    end
-    println(io, " done!")
-    ImageTracer(image, camera, samples_per_side=samples_per_side)
-end
-
-"""
-    demo(; output_file::String = "demo.jpg",
-           camera_type::Type{<:Camera} = PerspectiveCamera,
-           camera_position::Tuple{Float32, Float32, Float32} = (-3f0, 0f0, 0f0),
-           camera_orientation::Tuple{Float32, Float32, Float32} = (0f0, 0f0, 0f0),
-           screen_distance::Float32 = 2f0,
-           image_resolution::Tuple{Int, Int} = (540,540),
-           samples_per_side::Int = 0,
-           renderer_type::Type{<:Renderer} = PathTracer,
-           pt_n::Int = 10,
-           pt_max_depth::Int = 2,
-           pt_roulette_depth::Int = 3,
-           α::Float32 = 0.75f0,
-           γ::Float32 = 1f0,
-           luminosity::Union{Float32, Nothing} = nothing,
-           use_threads::Bool = true,
-           disable_output::Bool = false)
-
-Create a demo image.
-
-# Arguments
-
-- `output_file`: output LDR file name (the HDR file will have the same name, but with "pfm" extension).
-- `camera_type`: choose camera type ("perspective" or "orthogonal").
-- `camera_position`: camera position in the scene as "X,Y,Z".
-- `camera_orientation`: camera orientation as "angX,angY,angZ".
-- `screen_distance`: only for "perspective" camera: distance between camera and screen.
-- `image_resolution`: resolution of the rendered image.
-- `renderer_type`: type of renderer to use ("onoff", "flat" or "path").
-- `samples_per_side`: number of samples per pixel (must be a perfect square).
-- `pt_n`: number of rays fired for mc integration (path-tracer only).
-- `pt_max_depth`: maximum number of reflections for each ray (path-tracer only).
-- `pt_roulette_depth`: depth of the russian-roulette algorithm (path-tracer only).
-- `α`: scaling factor for the normalization process.
-- `γ`: gamma value for the tone mapping process.
-- `luminosity`: image luminosity for the tone mapping process; if `nothing`, use mean luminosity. Use for better tuning.
-- `use_threads`: use macro `Threads.@threads`.
-- `disable_output`: if `true`, no message is printed.
-"""
-function demo(; output_file::String = "demo.jpg",
-                camera_type::Type{<:Camera} = PerspectiveCamera,
-                camera_position::Tuple{Float32, Float32, Float32} = (-3f0, 0f0, 0f0),
-                camera_orientation::Tuple{Float32, Float32, Float32} = (0f0, 0f0, 0f0),
-                screen_distance::Float32 = 2f0,
-                image_resolution::Tuple{Int, Int} = (540,540),
-                renderer_type::Type{<:Renderer} = PathTracer,
-                samples_per_side::Int = 0,
-                pt_n::Int = 10,
-                pt_max_depth::Int = 2,
-                pt_roulette_depth::Int = 3,
-                α::Float32 = 0.75f0,
-                γ::Float32 = 1f0,
-                luminosity::Union{Float32, Nothing} = nothing,
-                use_threads::Bool = true,
-                disable_output::Bool = false)
-    renderer = demo_load_scene(renderer_type, pt_n, pt_max_depth, pt_roulette_depth)
-    image_tracer = demo_load_tracer(image_resolution, camera_type, camera_position, camera_orientation, screen_distance, samples_per_side, disable_output=disable_output)
-    output_file_hdr = join([split(output_file, ".")[begin:end-1]..., "pfm"], ".")
-    output_file_ldr = output_file
-    render(image_tracer=image_tracer, renderer=renderer, output_file=output_file_hdr, use_threads=use_threads, disable_output=disable_output)
-    tonemapping(input_file=output_file_hdr, output_file=output_file_ldr, α=α, γ=γ, luminosity=luminosity, disable_output=disable_output)
-end
-
-
 function render_scene_from_script(input_script::String,
                                   ; output_file::String = "out.jpg",
                                     α::Float32 = 0.75f0,
